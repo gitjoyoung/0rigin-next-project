de786bb14631b4f218c7825f29ce52e6da0071f5
Jo Young <githuborigin@gmail.com>
2024-01-04 03:51:40 +0900

    fix:레이아웃의 main 태그로 감싸줌

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "./../components/Footer/Footer";
 import Contact from "./../components/Contact/Contact";
-
 export const metadata = {
   title: "origin project",
   description: "오리진 프로젝트",
@@ -11,14 +10,14 @@ export const metadata = {
 };
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="ko">
       <body>
-        <Header />
-
-        {children}
-
-        <Contact />
-        <Footer />
+          <main>
+            <Header />
+            {children}
+            <Contact />
+            <Footer />
+          </main>
       </body>
     </html>
   );
