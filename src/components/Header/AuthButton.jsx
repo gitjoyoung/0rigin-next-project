76d6b93bcf1300c96aeed640fc3bcf5946d6f749
Jo Young <githuborigin@gmail.com>
2024-01-03 22:01:51 +0900

    feat:유저 인증 버튼 컴포넌트  생성

--- src/components/Header/AuthButton.jsx
+++ src/components/Header/AuthButton.jsx
@@ -1,22 +1,29 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-export default function AuthButton() {
+export default function AuthButton({ name }) {
+  const router = useRouter();
+
   const [isLoggedIn, setLoggedIn] = useState(false);
   return (
-    <div className="flex gap-3">
+    <div className=" text-sm flex gap-3 w-48 justify-end">
       {isLoggedIn ? (
-        <p>{"테스터님 반갑습니다"}</p>
+        <div>
+          <p>{`${name}님 반갑습니다`}</p>
+          <div className="flex gap-2 justify-end">
+            <button className="text-xs" onClick={() => setLoggedIn(false)}>
+              로그아웃
+            </button>
+            <button className="text-xs">마이페이지</button>
+          </div>
+        </div>
       ) : (
         <>
-          <button
-            className="border border-black p-1 "
-            type="button"
-            onClick={() => setLoggedIn(true)}
-          >
+          <button className=" text-sm" onClick={() => setLoggedIn(true)}>
             로그인 버튼
           </button>
-          <button className="btn">회원가입</button>
+          <button className=" text-sm">회원가입</button>
         </>
       )}
     </div>
