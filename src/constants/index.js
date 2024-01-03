db01471bc0878290727cfca62e95ccf0187bc6fc
Jo Young <githuborigin@gmail.com>
2024-01-03 22:00:55 +0900

    feat: 게시판 카테코리 상수 선언

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const headerNav = [
+  {
+    title: "소개",
+    url: "/intro",
+  },
+  {
+    title: "게시판",
+    url: "/read",
+  },
+  {
+    title: "게임",
+    url: "/site",
+  },
+  {
+    title: "contact",
+    url: "/contact",
+  },
+];
