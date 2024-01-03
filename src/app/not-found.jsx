5903ef8362bc2956920a65bcaaf4d48f4c26ca6f
Jo Young <githuborigin@gmail.com>
2024-01-03 22:02:05 +0900

    feat:404 에러페이지 생성

---
+++ src/app/not-found.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">404 - 페이지가 없습니다...</h1>
+      <p className="text-gray-600 mb-8">
+        죄송합니다. 찾고 계신 페이지가 존재하지 않습니다.
+      </p>
+      <Link href={"/"} className="bg-cyan-500 text-white px-4 py-2 rounded-md hover:bg-cyan-600 focus:outline-none focus:ring focus:border-blue-300">
+        홈으로 돌아가기
+      </Link>
+    </div>
+  );
+}
