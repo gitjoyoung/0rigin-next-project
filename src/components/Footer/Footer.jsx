8443871b161ace579288fd68abca5ee83d6282f1
Jo Young <githuborigin@gmail.com>
2024-01-03 22:01:09 +0900

    feat: 푸터 생성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex items-center justify-center bg-gray-600 h-20 w-full">
-      <h1>Footer</h1>
+    <div className="flex items-center justify-center bg-gray-300 h-[10vh] w-full">
+      <p>FooCopyright © Origin Corp. All Rights Reserved.</p>
     </div>
   );
 }
