418ccde2e7d7c644f5657711de5bc5ca0b54bd43
Jo Young <githuborigin@gmail.com>
2024-01-10 19:54:27 +0900

    fix:회원가입 동적 라우터 설정

---
+++ src/app/sign/[slug]/page.js
@@ -0,0 +1,18 @@
+"use client";
+
+import Sign from "@/components/Auth/Sign/Sign";
+import TermsOfService from "@/components/Auth/Sign/TermsOfService";
+import { useState } from "react";
+
+export default function Page() {
+  const [changePage, setChangePage] = useState(false);
+
+  const handleAcceptTerms = () => {
+    setChangePage(true);
+  };
+  if (changePage) {
+    return <Sign />;
+  } else {
+    return <TermsOfService onTermsAgreed={handleAcceptTerms} />;
+  }
+}
--- src/app/sign/page.jsx
+++ src/app/sign/page.jsx
@@ -1,33 +1,16 @@
-"use client";
-import React from "react";
+// pages/sign/[[...slug]].js
 
-export default function sign() {
+import Link from "next/link";
+
+const SignPage = ({ slug }) => {
   return (
-    <div className="border p-20 flex flex-col gap-6 m-20">
-      <h1 className="font-bold flex justify-center text-2xl">회원가입</h1>
-      <form
-        className="flex flex-col gap-3"
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(e.target.id.value, e.target.password.value);
-        }}
-      >
-        <input
-          name="id"
-          key="id"
-          type="text"
-          placeholder="ID"
-          className="border border-gray-300 p-2"
-        />
-        <input
-          name="password"
-          key="password"
-          type="password"
-          placeholder="Password"
-          className="border border-gray-300 p-2"
-        />
-        <button className="mt-3 p-2">회원가입</button>
-      </form>
+    <div>
+      <h1> 오리진 커뮤니티에 오신 것을 환영합니다</h1>
+      <h3>회원가입을 하시겠습니까?</h3>
+      <Link href={"/"}>이미 회원입니다 로그인 페이지</Link>
+      <Link href={"sign/signup"}>네 할게요</Link>
     </div>
   );
-}
+};
+
+export default SignPage;
