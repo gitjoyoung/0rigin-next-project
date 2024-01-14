f1cc4b2b79861a3217c7be924d09c4d8a852b95c
Jo Young <githuborigin@gmail.com>
2024-01-14 23:07:48 +0900

    fix:레이아웃 폰트 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Control } from "./Control";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "./../components/Footer/Footer";
-import Contact from "./../components/Contact/Contact";
+import { inter } from '@/app/ui/fonts';
 export const metadata = {
   title: "origin project",
   description: "오리진 프로젝트",
@@ -12,12 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-          <main>
-            <Header />
-            {children}
-            <Contact />
-            <Footer />
-          </main>
+
+        <Header />
+        <main className={`${inter.className} antialiased min-h-[100vh] w-full `}>{children}</main>
+        <Footer />
       </body>
     </html>
   );
