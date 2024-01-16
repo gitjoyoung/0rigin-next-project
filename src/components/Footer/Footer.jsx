dfcc8497def0590dae67e0a4cd763aee6b8c4bf6
Jo Young <githuborigin@gmail.com>
2024-01-16 11:47:05 +0900

    feat: 푸터

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
+  const contactList = [
+    { name: "1:1문의", link: "/" },
+    { name: "고객센터", link: "/" },
+    { name: "contact", link: "/" },
+    { name: "인재채용", link: "/" },
+  ];
+
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-300 h-[10vh] w-full">
-      <p >Origin project</p>
-      <p >Copyright © Origin Corp. All Rights Reserved.</p>
+    <div className="text-xs flex flex-col items-center justify-center gap-2  bottom-0  bg-gray-200 p-4 w-full">
+      <p className="">Origin project</p>
+      <ul className="  flex gap-4 flex-wrap font-semibold">
+        {contactList.map((item, index) => (
+          <li className="" key={index}>
+            <Link href={item.link}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <p>Copyright © Origin Corp. All Rights Reserved.</p>
     </div>
   );
 }
