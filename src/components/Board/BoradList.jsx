5473b6b61e06a96c34b5e402d88d0eb8baf18702
Jo Young <githuborigin@gmail.com>
2024-01-16 11:47:31 +0900

    feat: 게시물 리스트 컴포넌트

--- src/components/Board/BoradList.jsx
+++ src/components/Board/BoradList.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 export default function BoardList(props) {
-  const { title, timestamp, views, likes, comments, id , nickname } = props;
+  const { title, timestamp, views, likes, comments, id, nickname } = props;
   return (
     <Link href={`/board/read/${id}`}>
       <div className="border-t flex items-center justify-between flex-wrap  border-black border-dotted border-b-1 p-3">
@@ -12,8 +12,8 @@ export default function BoardList(props) {
         </div>
         <div className="flex flex-wrap">
           <div className="flex gap-2 text-xs text-gray-500">
-         닉네임 {nickname}<span>|</span>
-           작성일 {timestamp} <span>|</span> 조회 {views}
+            닉네임 {nickname}<span>|</span>
+            작성일 {timestamp} <span>|</span> 조회 {views}
             <span>|</span> 추천 {likes}
             <span>|</span> 950107
           </div>
