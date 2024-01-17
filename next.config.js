10f92312e10b1369ab176ce16cdc40d6b274a6e0
Jo Young <githuborigin@gmail.com>
2024-01-18 08:35:48 +0900

    feat: i18n 국제화 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    // sw 국제화 설정 html lang = "ko"
+    i18n: {
+        locales: ['ko'],
+        defaultLocale: 'ko',
+    },
 };
 
 module.exports = nextConfig;
