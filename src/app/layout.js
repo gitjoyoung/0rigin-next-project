331348236544c8184d24e6308539c7349965d1b4
Jo Young <githuborigin@gmail.com>
2024-01-18 08:36:43 +0900

    feat: i18n 국제화 설정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,23 @@
-import { Control } from "./Control";
-import "./globals.css";
-import Header from "@/components/Header/Header";
-import Footer from "./../components/Footer/Footer";
+import { Control } from './Control';
+import './globals.css';
+import Header from '@/components/Header/Header';
+import Footer from './../components/Footer/Footer';
 import { inter } from '@/app/ui/fonts';
 export const metadata = {
-  title: "origin project",
-  description: "오리진 프로젝트",
-  keywords: ["origin"],
+    title: 'origin project',
+    description: '오리진 프로젝트',
+    keywords: ['origin'],
 };
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ko">
-      <body>
-
-        <Header />
-        <main className={`${inter.className} antialiased min-h-[100vh] w-full `}>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body>
+                <Header />
+                <main className={`${inter.className} antialiased min-h-[100vh] w-full `}>
+                    {children}
+                </main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
