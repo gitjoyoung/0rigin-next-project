1f41cd5e1dd69f7d2f7b6b604baaa16f751b62f0
Jo Young <githuborigin@gmail.com>
2024-01-22 23:20:42 +0900

    feat :awesome font 넥스트  설정

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,21 +3,25 @@ import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from './../components/Footer/Footer';
 import { inter } from '@/app/ui/fonts';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 export const metadata = {
-    title: 'origin project',
-    description: '오리진 프로젝트',
-    keywords: ['origin'],
+   title: '0rigin project',
+   description: '제로리진 프로젝트',
+   keywords: ['origin', '0rigin'],
 };
 export default function RootLayout({ children }) {
-    return (
-        <html>
-            <body>
-                <Header />
-                <main className={`${inter.className} antialiased min-h-[100vh] w-full `}>
-                    {children}
-                </main>
-                <Footer />
-            </body>
-        </html>
-    );
+   return (
+      <html>
+         <body>
+            <Header />
+            <main className={`${inter.className} antialiased min-h-[100vh] w-full `}>
+               {children}
+            </main>
+            <Footer />
+         </body>
+      </html>
+   );
 }
