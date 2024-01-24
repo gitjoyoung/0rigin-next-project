d8080cfe5e8c963755a583281eff7cff66198324
Jo Young <githuborigin@gmail.com>
2024-01-24 14:36:05 +0900

    feat:0rigin 소개 페이지

---
+++ src/app/introduce/page.tsx
@@ -0,0 +1,10 @@
+import Introduce from '@/components/introduce/Introduce';
+import React from 'react';
+
+export default function page() {
+   return (
+      <>
+         <Introduce />
+      </>
+   );
+}
