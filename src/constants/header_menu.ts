7d460f381be72f018aee845785b8bb80079010f9
Jo Young <githuborigin@gmail.com>
2024-01-26 22:19:07 +0900

    fix: 헤더 상수

---
+++ src/constants/header_menu.ts
@@ -0,0 +1,20 @@
+const headerNav = [
+   {
+      title: '소개',
+      url: '/introduce',
+   },
+   {
+      title: '게시판',
+      url: '/board',
+   },
+   {
+      title: '마이페이지',
+      url: '/mypage',
+   },
+   {
+      title: 'contact',
+      url: '/contact',
+   },
+]
+
+export default headerNav
