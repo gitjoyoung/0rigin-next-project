52d218e1b2b18be7f2e65f78a515476ecce42c56
Jo Young <githuborigin@gmail.com>
2024-01-29 18:42:15 +0900

    fix : footer 상수관리 및 ts 변경

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import footerNav from '@/constants/Main/footerNav'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Footer() {
+   return (
+      <div className="text-xs flex flex-col items-center justify-center gap-2  bottom-0  bg-gray-200 p-4 w-full">
+         <p className="">Origin project</p>
+         <ul className="  flex gap-4 flex-wrap font-semibold">
+            {footerNav.map(({ name, link, id }) => (
+               <li className="" key={id}>
+                  <Link href={link}>{name}</Link>
+               </li>
+            ))}
+         </ul>
+         <p>Copyright © Origin Corp. All Rights Reserved.</p>
+      </div>
+   )
+}
