2ef0823836cec9f3510e85243882b2fde81bb0ab
Jo Young <githuborigin@gmail.com>
2024-02-01 14:33:46 +0900

    fix : 소개글  변경

--- src/components/introduce/Introduce.tsx
+++ src/components/introduce/Introduce.tsx
@@ -24,7 +24,7 @@ export default function Introduce() {
 
          <div
             ref={sectionsRef.story}
-            className="flex flex-col items-center max-w-[600px] gap-3 h-[100vh] justify-center  text-center"
+            className="flex flex-col items-center max-w-[600px] gap-3 h-[100vh] justify-center  text-center animate-fadeIn "
          >
             <Image
                src="/mascot/winksaurus.png"
@@ -33,7 +33,7 @@ export default function Introduce() {
                height={200}
             />{' '}
             <h1 className="text-2xl font-bold">{INTRODUCE_TEXT.story.title}</h1>
-            <p className="break-words break-keep">
+            <p className="break-words break-keep px-3">
                {INTRODUCE_TEXT.story.body}
             </p>
          </div>
@@ -43,10 +43,10 @@ export default function Introduce() {
             ref={sectionsRef.welcome}
             className="flex flex-col items-center justify-center max-w-[600px] gap-3 h-[100vh]"
          >
-            <h1 className="text-2xl font-bold">
+            <h1 className="text-2xl font-bold px-3">
                {INTRODUCE_TEXT.welcome.title}
             </h1>
-            <p className="break-words break-keep">
+            <p className="break-words break-keep px-3">
                {INTRODUCE_TEXT.welcome.body}
             </p>
          </div>
@@ -56,10 +56,10 @@ export default function Introduce() {
             ref={sectionsRef.numbers}
             className="flex flex-col items-center justify-center gap-3 w-full h-[100vh]"
          >
-            <h1 className="text-2xl font-bold">
-               {INTRODUCE_TEXT.numbers.body}
+            <h1 className="text-2xl font-bold px-3">
+               {INTRODUCE_TEXT.numbers.title}
             </h1>
-            <p> {INTRODUCE_TEXT.numbers.body} </p>
+            <p className="px-3"> {INTRODUCE_TEXT.numbers.body} </p>
             <p>마지막 업데이트: 24.04.23</p>
             <CommunityStats />
          </div>
@@ -68,7 +68,7 @@ export default function Introduce() {
             <div className="flex flex-col gap-1">
                {Object.entries(sectionsRef).map(([key, ref]) => (
                   <button
-                     className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-300 focus:outline-none focus:ring  border-white"
+                     className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-300 focus:outline-none focus:ring border-white hover:scale-105 transition-transform duration-300"
                      key={key}
                      type="button"
                      onClick={() => scrollToSection(ref)}
