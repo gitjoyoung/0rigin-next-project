31eefa9d19a96e280e9203f27e3351b02717e7e8
Jo Young <githuborigin@gmail.com>
2024-02-02 17:26:35 +0900

    feat: 홈 상단 tiker 구상

--- src/components/Header/Ticker.tsx
+++ src/components/Header/Ticker.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+'use client'
 
-export default function Ticker() {
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+   point?: number
+   visitor?: number
+   post?: number
+   member?: number
+}
+
+export default function Ticker({
+   point = 0,
+   visitor = 0,
+   post = 0,
+   member = 0,
+}: Props) {
+   const [backAnimation, setBackAnimation] = useState('')
+
+   /** 애니메이션 모션 효과 */
+   useEffect(() => {
+      setBackAnimation('animate-pulse ')
+      const timer = setTimeout(() => {
+         setBackAnimation('')
+      }, 2000)
+
+      return () => clearTimeout(timer)
+   }, [point, visitor, post, member])
+
    return (
-      <aside className=" px-2 flex gap-2 justify-between bg-black h-4 w-full text-white text-xs">
-         <div className="">
-            <p>포인트 현황 1000m</p>
+      <aside
+         className={`px-2 flex gap-2 justify-between ${backAnimation} bg-black fill-in-animation items-center w-full text-white text-xs`}
+      >
+         <div className="flex ">
+            <p className="border-r px-2">익일 포인트 현황 {point}</p>
+            <p className="border-r px-2">수령 된 포인트 현황 {point}</p>
          </div>
-         <div className="flex">
-            <p>오늘 방문자 100</p>
-            <p>게시글 1000+</p>
-            <p>회원 수 1000</p>
+         <div className="flex ">
+            <p className="border-r px-2">오늘 방문자 {visitor}</p>
+            <p className="border-r  px-2">게시글 {post}</p>
+            <p className="px-2">회원 수 {member}</p>
          </div>
       </aside>
    )
