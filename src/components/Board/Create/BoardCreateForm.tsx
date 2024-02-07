021029daaee605c342839f2f0defda6251c4ed07
Jo Young <githuborigin@gmail.com>
2024-02-07 13:51:37 +0900

    feat: 글쓰기 블록 로직으로 변경 개선작업중

--- src/components/Board/Create/BoardCreateForm.tsx
+++ src/components/Board/Create/BoardCreateForm.tsx
@@ -7,6 +7,7 @@ import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/navigation'
 import ImagePicker from './ImagePicker'
 import dayjs from 'dayjs'
+import fetchCreateForm from '@/app/api/board/create'
 
 interface Props {
    nickname?: string
@@ -30,14 +31,55 @@ export default function BoardCreateForm({
 
    const router = useRouter()
    const [showPassword, setShowPassword] = useState(false)
-   const editorRef = useRef(null)
    const [imageFiles, setImageFiles] = useState([])
-   const [text, setText] = useState('')
+
+   const [divs, setDivs] = useState([{ id: `div-0`, content: '' }])
+   const divRefs = useRef([]) // 모든 div에 대한 refs를 저장하는 배열
+
+   useEffect(() => {
+      if (divRefs.current[divs.length - 1]) {
+         divRefs.current[divs.length - 1].focus()
+      }
+   }, [divs])
+
+   const addDiv = () => {
+      const newDivId = `div-${divs.length}`
+      setDivs([...divs, { id: newDivId, content: '' }])
+      divRefs.current.push(null)
+   }
+
+   const deleteDiv = (index) => {
+      if (divs.length > 1 && divs[index].content === '') {
+         const newDivs = [...divs]
+         newDivs.splice(index, 1)
+         setDivs(newDivs)
+         divRefs.current.splice(index, 1)
+      }
+   }
+
+   const handleKeyPress = (e, index) => {
+      if (e.key === 'Enter') {
+         e.preventDefault() // 기본 엔터 동작 방지
+         if (index === divs.length - 1) {
+            addDiv()
+         } else {
+            divRefs.current[index + 1].focus()
+         }
+      } else if (e.key === 'Backspace' && divs.length > 1 && index !== 0) {
+         const currentContent = divRefs.current[index].innerText.trim()
+         if (currentContent === '') {
+            e.preventDefault()
+            deleteDiv(index)
+         }
+      }
+   }
 
    const handleShowPassword = () => {
       setShowPassword(!showPassword)
    }
-
+   /**
+    * 글쓰기 폼을 서버로 전송하는 함수
+    */
    const fetchCreate = async (e) => {
       e.preventDefault()
 
@@ -46,29 +88,15 @@ export default function BoardCreateForm({
          nickname: e.target.nickname.value,
          password: e.target.password.value,
          title: e.target.title.value,
-         body: editorRef.current.innerHTML,
-         imageFiles,
-         timestamp,
+         body: divs,
+         timestamp: Number(timestamp), // Convert the timestamp to a number
          isPublic: true,
          category: 'any',
       }
 
-      const options = {
-         url: `${process.env.NEXT_PUBLIC_API_URL}board`,
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         data: JSON.stringify(dataObject),
-      }
-
-      try {
-         const res = await axios(options)
-         const { data } = res
-         router.push(`/board/read/${data.id}`)
-      } catch (error) {
-         console.log('error :', error)
-      }
+      await fetchCreateForm(dataObject).then((data) =>
+         router.push(`/board/read/${data.id}`),
+      )
    }
 
    return (
@@ -118,12 +146,22 @@ export default function BoardCreateForm({
                setImageFiles={setImageFiles}
             />
             {/* 내용 contentEditTable 로 태그를 추가함 */}
-            <div
-               ref={editorRef}
-               contentEditable="true"
-               className="w-full min-h-[375px] py-5 pb-10 px-4 border"
-            />
-
+            <div className="border flex flex-col gap-1 p-3 pb-10 min-h-[300px]">
+               {divs.map((div, index) => (
+                  <p
+                     key={div.id}
+                     contentEditable="true"
+                     ref={(el) => {
+                        divRefs.current[index] = el
+                     }} // 각 div에 대한 ref를 설정
+                     onKeyDown={(e) => handleKeyPress(e, index)}
+                     className="hover:border-gray-500 w-full  resize-none  overflow-y-auto"
+                     suppressContentEditableWarning
+                  >
+                     {div.content}
+                  </p>
+               ))}
+            </div>
             {/* 제출 버튼 */}
             <div className="flex gap-6 justify-end p-1 mt-3 mb-3">
                <button
