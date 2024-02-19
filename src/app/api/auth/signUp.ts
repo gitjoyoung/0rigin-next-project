6a5f3ed58175cf1bf1de45f8ce8aec72e74c61b1
Jo Young <githuborigin@gmail.com>
2024-02-19 20:30:15 +0900

    fix:파이어폭스 로그인 수정

--- src/app/api/auth/signUp.ts
+++ src/app/api/auth/signUp.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-
+import { auth } from '@/lib/firebase'
+import {  createUserWithEmailAndPassword } from 'firebase/auth'
 type UserData = {
    userid: string
    password: string
@@ -10,10 +10,19 @@ type UserData = {
  * @param userData
  */
 const fetchSignUp = async (userData: UserData) => {
+   console.log('실행')
    try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}users`, userData)
+      await createUserWithEmailAndPassword(
+         auth,
+         userData.userid,
+         userData.password,
+      ).then((userCredential) => {
+         const { user } = userCredential
+         console.log(user)
+      })
+      console.log('서버 통신 성공')
    } catch (error) {
-      console.log('실패 :회원가입에 실패 하였습니다.')
+      console.log(error, ' 실패 :회원가입에 실패 하였습니다.')
    }
 }
 
