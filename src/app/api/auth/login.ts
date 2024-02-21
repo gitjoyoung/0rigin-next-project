582c6cc590edf8da6f3eac68d3489f07af3d1b14
Jo Young <githuborigin@gmail.com>
2024-02-21 23:00:54 +0900

    feat: 로그인 통신 함수 추가

--- src/app/api/auth/login.ts
+++ src/app/api/auth/login.ts
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import { auth } from '@/lib/firebase'
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
 
 interface Login {
    id: string
@@ -9,21 +10,31 @@ interface Login {
  * @param userId
  * @param password
  */
-const fetchLogin = async (userId, password): Promise<Login | null> => {
-   const options = {
-      url: `${process.env.NEXT_PUBLIC_API_URL}users?userid=${userId}&password=${password}`,
-      method: 'get',
-   }
-   try {
-      const res = await axios(options)
-      if (res.data.length > 0) {
-         return res.data // 로그인 성공 시 사용자 데이터 반환
-      }
-      return null // 로그인 실패 시 null 반환
-   } catch (error) {
-      console.error(`로그인 요청 오류: ${error}`)
-      return null // 오류 발생 시 null 반환
-   }
+export const fetchLogin = (userId, password): Promise<Login | null> => {
+   const email = userId.includes('@') ? userId : `${userId}@0rigin.com`
+   console.log('fetchLogin', userId, email, password)
+   return signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+         console.log(userCredential)
+         // 필요한 경우 userCredential.user에서 Login 타입 객체로 변환
+         const loginResult: Login = {
+            id: userCredential.user.uid,
+            name: userCredential.user.displayName || '사용자 이름',
+         }
+         return loginResult // 로그인 성공 시 Login 객체 반환
+      })
+      .catch((error) => {
+         console.error(`로그인 요청 오류: ${error}`)
+         return null // 오류 발생 시 null 반환
+      })
 }
 
-export default fetchLogin
+export const fetchLogout = () => {
+   return signOut(auth)
+      .then(() => {
+         console.log('로그아웃 성공')
+      })
+      .catch((error) => {
+         console.error(`로그아웃 요청 오류: ${error}`)
+      })
+}
--- src/app/api/auth/signUp.ts
+++ src/app/api/auth/signUp.ts
@@ -1,29 +1,31 @@
 import { auth } from '@/lib/firebase'
-import {  createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 type UserData = {
    userid: string
    password: string
    gender: string
 }
+
 /**
  * 회원가입 요청
  * @param userData
  */
-const fetchSignUp = async (userData: UserData) => {
-   console.log('실행')
-   try {
-      await createUserWithEmailAndPassword(
-         auth,
-         userData.userid,
-         userData.password,
-      ).then((userCredential) => {
+const fetchSignUp = (userData: UserData) => {
+   return createUserWithEmailAndPassword(
+      auth,
+      userData.userid,
+      userData.password,
+   )
+      .then((userCredential) => {
          const { user } = userCredential
-         console.log(user)
+         return user
       })
-      console.log('서버 통신 성공')
-   } catch (error) {
-      console.log(error, ' 실패 :회원가입에 실패 하였습니다.')
-   }
+      .catch((error) => {
+         console.log(error, ' 실패 :회원가입에 실패 하였습니다.')
+         // 에러 처리 로직
+         return null
+      })
 }
 
 export default fetchSignUp
