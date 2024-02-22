8897002a8bda2ba731b9adab7ec0f1aa0893ce20
Jo Young <githuborigin@gmail.com>
2024-02-22 22:46:17 +0900

    fix: 타이스크립트 설정파일

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   darkMode: 'class',
+   content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+
+   theme: {
+      extend: {
+         keyframes: {
+            from: { transform: 'rotateY(0deg) scale(2.0)' },
+            to: { transform: 'rotateY(360deg) scale(1.0)' },
+         },
+         animation: {
+            intro: 'intro 2s ease-in-out',
+         },
+         backgroundImage: {
+            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            'gradient-conic':
+               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+         },
       },
-    },
-  },
-  plugins: [],
-};
+   },
+   plugins: [],
+}
