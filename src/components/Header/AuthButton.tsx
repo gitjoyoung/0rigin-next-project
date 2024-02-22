f78a29421455afbac3346be0061efb02420edb5b
Jo Young <githuborigin@gmail.com>
2024-02-22 22:36:57 +0900

    fix: 헤더 수정

--- src/components/Header/AuthButton.tsx
+++ src/components/Header/AuthButton.tsx
@@ -1,30 +1,58 @@
 'use client'
 
+import { fetchLogout } from '@/app/api/auth/login'
+import { auth } from '@/lib/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function AuthButton({ name }) {
+export default function AuthButton() {
    const router = useRouter()
+   const [userId, setUserId] = useState('')
+   const [isLoggedIn, setIsLoggedIn] = useState(false)
+   const [isLoading, setIsLoading] = useState(true)
 
-   const [isLoggedIn, setLoggedIn] = useState(false)
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+         if (user) {
+            setUserId(user.email)
+            setIsLoggedIn(true)
+         } else {
+            setIsLoggedIn(false)
+         }
+         // 로그인 상태 확인이 완료되면 로딩 상태를 false로 설정
+         setIsLoading(false)
+      })
+
+      return () => unsubscribe()
+   }, [])
+
+   if (isLoading) {
+      return <div>로딩 중...</div> // 또는 로딩 인디케이터를 표시할 수 있습니다.
+   }
+
    return (
-      <div className=" text-sm flex gap-3 items-center  justify-end">
+      <div className=" text-sm flex gap-3 items-center ">
          {isLoggedIn ? (
-            <>
-               <p>{`${name}님 반갑습니다`}</p>
+            <div className="flex-col">
+               <p>{`${userId}`}</p>
                <div className="flex gap-2 justify-end">
                   <button
                      type="button"
-                     className="text-xs"
-                     onClick={() => setLoggedIn(false)}
+                     className="text-xs "
+                     onClick={fetchLogout}
                   >
                      로그아웃
                   </button>
-                  <button type="button" className="text-xs">
+                  <button
+                     type="button"
+                     onClick={() => router.push('/mypage')}
+                     className="text-xs"
+                  >
                      마이페이지
                   </button>
                </div>
-            </>
+            </div>
          ) : (
             <>
                <button
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,16 @@ import React, { Fragment, useState } from 'react'
 import headerNav from '@/constants/home/headerNav'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBurger, faX, faBell } from '@fortawesome/free-solid-svg-icons'
-import { Transition } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import AuthButton from './AuthButton'
 import SearchBox from './SearchBox'
+import NavModal from './NavModal'
 
 export default function Header() {
-   const [isMenuOpen, setIsMenuOpen] = useState(false)
-   const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen)
-   }
+   // 모달 상태
+   const [isModalOpen, setIsModalOpen] = useState(false)
+   // 모달 스위치
+   const toggleModal = () => setIsModalOpen((prevState) => !prevState)
 
    return (
       <header className="flex justify-between p-1 sm:p-3 items-end  w-full border-b mt-1 mb-2 border-black  shadow-md">
@@ -21,100 +22,62 @@ export default function Header() {
          <Link href="/">
             <h1 className="md:text-4xl text-xl font-bold ">0rigin</h1>
          </Link>
-
-         {/* 메뉴 리스트 */}
-         <div className="flex-1 hidden md:flex justify-between ml-10">
-            <nav className="flex items-end gap-8 text-gray-600 ">
-               {headerNav.map(({ title, url }) => (
-                  <Link key={title} href={url}>
-                     <p className="hover:text-gray-900 hover:font-semibold text-md font-normal ">
-                        {title}
-                     </p>
-                  </Link>
-               ))}
-            </nav>
-         </div>
+         {/* 메뉴 네비게이터 */}
+         <nav className="flex-1 hidden md:flex  ml-10 flex items-end gap-8 text-gray-600 ">
+            {headerNav.map(({ title, url }) => (
+               <Link key={title} href={url}>
+                  <p className="hover:text-gray-900 hover:font-semibold text-md font-normal ">
+                     {title}
+                  </p>
+               </Link>
+            ))}
+         </nav>
          {/* 서치바 */}
          <div className="flex-1 max-w-48 mx-3">
             <SearchBox />
          </div>
-
-         {/* 로그인 버튼 */}
+         {/* 반응형 네비게이션 */}
          <div className="flex items-end gap-5">
-            {/* 로그인 회원가입 버튼 모바일에서 숨김 처리 */}
+            {/* PC 화면 로그인,  회원가입 */}
             <div className="hidden md:flex ">
-               <AuthButton name={undefined} />
+               <AuthButton />
             </div>
 
-            {/* 모바일 화면 버튼 */}
-            <div className="md:hidden flex gap-1 items-center">
+            {/* MOBILE 화면  버튼 */}
+            <div className="md:hidden  flex gap-2 items-center">
+               {/* 알림버튼 개발 중 */}
                <button
+                  className="flex gap-1"
                   type="button"
-                  aria-label="알림"
-                  onClick={() => console.log('알림')}
+                  aria-label="알림 버튼"
                >
+                  <p className="text-xs">{1}</p>{' '}
                   <FontAwesomeIcon icon={faBell} size="lg" />
                </button>
-               <button
-                  className="border-none"
-                  type="button"
-                  onClick={toggleMenu}
-                  aria-label="메뉴 토글"
-               >
-                  <FontAwesomeIcon icon={faBurger} size="lg" />
-               </button>
+               <div className="relative">
+                  <button
+                     className="border p-1 "
+                     type="button"
+                     onClick={toggleModal}
+                     aria-label="메뉴 버튼"
+                  >
+                     <FontAwesomeIcon icon={faBurger} size="lg" />
+                  </button>
+                  {/* 모달 애니메이션  */}
+                  <NavModal
+                     isModalOpen={isModalOpen}
+                     toggleModal={toggleModal}
+                  />
+               </div>
             </div>
          </div>
 
-         {/* 배경 클릭으로 메뉴 닫기 */}
+         {/* 모달 배경 */}
          <div
-            className={`fixed inset-0 z-40 ${isMenuOpen ? 'block' : 'hidden'}`}
-            onClick={toggleMenu}
+            className={`fixed  inset-0 z-40 ${isModalOpen ? 'block' : 'hidden'}`}
+            onClick={toggleModal}
             aria-hidden="true"
          />
-
-         <Transition
-            as={Fragment}
-            show={isMenuOpen}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-         >
-            <div className="absolute top-[60px] right-0 w-2/4 max-w-sm border pb-3 border-black bg-white p-2 shadow-md z-50">
-               <div className="text-right">
-                  <button
-                     type="button"
-                     className="px-1"
-                     onClick={toggleMenu}
-                     aria-label="Toggle Menu"
-                  >
-                     <FontAwesomeIcon icon={faX} size="lg" />
-                  </button>
-               </div>
-               <div
-                  role="button"
-                  tabIndex={0}
-                  onClick={toggleMenu}
-                  onKeyDown={(e) => {
-                     if (e.key === 'Enter' || e.key === ' ') {
-                        toggleMenu()
-                     }
-                  }}
-               >
-                  <AuthButton name={undefined} />
-                  {headerNav.map(({ id, url, title }) => (
-                     <Link key={id} href={url}>
-                        <p className="hover:text-gray-900 hover:font-semibold textid,-md font-normal id2">
-                           {title}
-                        </p>
-                     </Link>
-                  ))}
-               </div>
-            </div>
-         </Transition>
       </header>
    )
 }
