279ed6e2b07c95cf5232c4d8d46243c741187058
Jo Young <githuborigin@gmail.com>
2024-02-22 22:42:25 +0900

    feat: 검색  컴포넌트 수정

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import SearchButton from '../Header/SearchButton'
+import SearchButton from '../Header/SearchBox'
 
 interface SearchResult {
    id: number
