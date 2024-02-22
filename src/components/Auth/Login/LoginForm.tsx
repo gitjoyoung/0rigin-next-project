e14cf607e7178a8c55db112eed97f5103e254c26
Jo Young <githuborigin@gmail.com>
2024-02-22 22:44:04 +0900

    fix: 로그인 페이지 구상중

--- src/components/Auth/Login/LoginForm.tsx
+++ src/components/Auth/Login/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import fetchLogin from '@/app/api/auth/login'
+import { fetchLogin } from '@/app/api/auth/login'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
@@ -8,6 +8,7 @@ import React, { useState } from 'react'
 export default function Login() {
    const router = useRouter()
 
+   // 로그인 form 정보
    const [id, setId] = useState('')
    const [password, setPassword] = useState('')
    const [error, setError] = useState('')
@@ -21,7 +22,7 @@ export default function Login() {
       e.preventDefault()
 
       const response = await fetchLogin(id, password)
-      if (response > 0) {
+      if (response) {
          router.push('/')
       } else {
          setError('아이디 또는 비밀번호가 일치하지 않습니다.')
@@ -29,15 +30,13 @@ export default function Login() {
    }
 
    return (
-      <section className=" w-full  flex justify-center  ">
+      <section className=" w-full  flex justify-center ">
          <div className="border p-10 flex justify-center items-center flex-col gap-6 m-3  w-full max-w-[500px] min-h-[500px]">
             <div>
                <h1 className="font-bold  text-2xl ">로그인</h1>
             </div>
-
             <form className="flex flex-col gap-3" onSubmit={handleLoginSubmit}>
                <input
-                  name="id"
                   key="id"
                   type="text"
                   placeholder="ID"
@@ -48,7 +47,6 @@ export default function Login() {
                   className={`border  p-2 `}
                />
                <input
-                  name="password"
                   key="password"
                   type="password"
                   placeholder="Password"
@@ -65,9 +63,14 @@ export default function Login() {
                   로그인
                </button>
             </form>
-            <div className=" text-sm flex gap-5  ">
-               <Link href="/login/forget">아이디 분실</Link>
-               <Link href="/login/forget">비밀번호 분실</Link>
+
+            <div className=" text-sm flex gap-5 ">
+               <Link className="border p-2" href="/sign">
+                  회원가입
+               </Link>
+               <Link className="border p-2" href="/login/forget">
+                  비밀번호 분실
+               </Link>
             </div>
          </div>
       </section>
