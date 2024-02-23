1f90d78560d354e08d7c86919f69d64c5e53478e
Jo Young <githuborigin@gmail.com>
2024-02-24 02:09:01 +0900

    feat: next-remove-imports  설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const removeImports = require('next-remove-imports')()
 const nextConfig = {
-    // sw 국제화 설정 html lang = "ko"
-    i18n: {
-        locales: ['ko'],
-        defaultLocale: 'ko',
-    },
-};
+   // sw 국제화 설정 html lang = "ko"
+   i18n: {
+      locales: ['ko'],
+      defaultLocale: 'ko',
+   },
+}
 
-module.exports = nextConfig;
+module.exports = removeImports({
+   ...nextConfig,
+})
