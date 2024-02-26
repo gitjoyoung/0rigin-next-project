cc28bd50b94ea72f9270c103318bb88cc011f278
Jo Young <githuborigin@gmail.com>
2024-02-26 23:05:08 +0900

    feat: 네비 버튼 컴포넌트 분리

---
+++ src/components/Board/Read/BoardNavButton.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function BoardNavButton({ postId, router }) {
+   return (
+      <div className="flex justify-between p-1 items-center">
+         <Link href={`/board/read/${parseInt(postId, 10) - 1} `}>
+            <p> 이전 글</p>
+         </Link>
+         <button
+            type="button"
+            className="px-2 py-2"
+            onClick={() => router.push('/board')}
+         >
+            목록
+         </button>
+         <Link href={`/board/read/${parseInt(postId, 10) + 1}`}>
+            <p>다음 글</p>
+         </Link>
+      </div>
+   )
+}
