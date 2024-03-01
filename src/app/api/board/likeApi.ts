06f9929cf7d0d18ccb9a9c5c990401593ae1492a
Jo Young <githuborigin@gmail.com>
2024-03-02 03:33:46 +0900

    feat: 좋아요 api 설정

--- src/app/api/board/likeApi.ts
+++ src/app/api/board/likeApi.ts
@@ -1,12 +1,39 @@
-import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { db } from '@/lib/firebase'
+import { doc, getDoc, increment, updateDoc } from 'firebase/firestore'
 
 // 댓글 추가
-export const addComment = async (postId, commentData) => {
-   const commentRef = doc(collection(db, 'posts', postId, 'comments'))
-   const newCommentData = {
-      ...commentData,
-      createdAt: serverTimestamp(),
+export const increaseLike = async (postId): Promise<number> => {
+   const postRef = doc(db, 'posts', postId) // 'posts' 컬렉션에서 postId에 해당하는 문서 참조 가져오기
+
+   try {
+      await updateDoc(postRef, {
+         like: increment(1), // 'likes' 필드의 현재 값에서 1 증가
+      })
+      const updatedDoc = await getDoc(postRef)
+      if (updatedDoc.exists()) {
+         console.log(' like:', updatedDoc.data().like) // 업데이트된 'like' 필드 값 출력
+         return updatedDoc.data().like // 업데이트된 'like' 필드 값 출력
+      }
+   } catch (error) {
+      console.error('Error updating likes:', error)
    }
-   await setDoc(commentRef, newCommentData)
-   return commentRef.id
+   return 0 // Add a return statement at the end of the function to return a default value of 0
+}
+
+export const increaseDislike = async (postId) => {
+   const postRef = doc(db, 'posts', postId) // 'posts' 컬렉션에서 postId에 해당하는 문서 참조 가져오기
+
+   try {
+      await updateDoc(postRef, {
+         dislike: increment(1), // 'dislikes' 필드의 현재 값에서 1 증가
+      })
+      const updatedDoc = await getDoc(postRef)
+      if (updatedDoc.exists()) {
+         console.log(' dislike:', updatedDoc.data()) // 업데이트된 'like' 필드 값 출력
+         return updatedDoc.data().dislike
+      }
+   } catch (error) {
+      console.error('Error updating dislikes:', error)
+   }
+   return 0
 }
