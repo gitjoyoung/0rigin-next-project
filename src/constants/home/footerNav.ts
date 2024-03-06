7e06a7bef4fa713eb49f61854729d90f5a57d179
Jo Young <githuborigin@gmail.com>
2024-03-06 09:21:26 +0900

    fix: 푸터 상수 변경

--- src/constants/home/footerNav.ts
+++ src/constants/home/footerNav.ts
@@ -1,8 +1,7 @@
 const FOOTER_NAV = Object.freeze([
    { id: 'inquiry', name: '1:1문의', link: '/' },
    { id: 'customerService', name: '고객센터', link: '/' },
-   { id: 'contact', name: 'contact', link: '/' },
-   { id: 'careers', name: '인재채용', link: '/' },
+   { id: 'contact', name: 'contact', link: '/contact' },
 ])
 
 export default FOOTER_NAV
