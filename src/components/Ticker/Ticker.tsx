726da83d7f7ec60174167a5ba08d34c5d7f9437a
Jo Young <githuborigin@gmail.com>
2024-03-06 09:10:46 +0900

    fix : 상단 ticker  출력 메세지 변경

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,61 +1,46 @@
 'use client'
 
+import { fetchInfo } from '@/app/api/board/infoApi'
 import React, { useEffect, useState } from 'react'
 
-interface Props {
-   point?: number
-   visitor?: number
-   post?: number
-   member?: number
-}
-
-export default function Ticker({
-   point = 100000,
-   visitor = 1111110,
-   post = 11110,
-   member = 1171,
-}: Props) {
+export default function Ticker() {
    const [backAnimation, setBackAnimation] = useState('')
+   const [count, setCount] = useState({
+      visitorCount: { count: 0 },
+      postCount: { count: 0 },
+      memberCount: { count: 0 },
+   })
 
-   /** 애니메이션 모션 효과 */
+   const fetchData = async () => {
+      try {
+         const data = await fetchInfo()
+         console.log('data', data)
+         // setCount(data) 이전 데이터를 유지하면서 새로운 데이터를 추가
+         setCount((prev) => ({ ...prev, ...data }))
+         console.log('count', count)
+      } catch (error) {
+         console.error('Failed to fetch data:', error)
+      }
+   }
+   // css
    useEffect(() => {
+      fetchData()
       setBackAnimation('animate-pulse ')
       const timer = setTimeout(() => {
          setBackAnimation('')
       }, 2000)
 
       return () => clearTimeout(timer)
-   }, [point, visitor, post, member])
-
-   const formatValue = (value: number): string => {
-      const suffixes = ['', 'K', 'M', 'B']
-      let index = 0
-      let updatedValue = value
-
-      while (updatedValue >= 1000 && index < suffixes.length - 1) {
-         updatedValue /= 1000
-         index += 1
-      }
-
-      const formattedValue = updatedValue.toFixed(1)
-      return formattedValue.endsWith('.0')
-         ? `${formattedValue.slice(0, -2)}${suffixes[index]}`
-         : `${formattedValue}${suffixes[index]}`
-   }
+   }, [])
 
    return (
       <aside
-         className={` flex gap-2 justify-between ${backAnimation} bg-black fill-in-animation items-center w-full text-white text-xs  `}
+         className={` flex gap-2 justify-end ${backAnimation} bg-black fill-in-animation items-center w-full text-white text-xs  `}
       >
          <div className="flex ">
-            <p className="border-r px-1">익일 포인트 :{formatValue(point)}</p>
-         </div>
-         <div className="flex ">
-            <p className="border-r border-l px-1 ">
-               방문자 {formatValue(visitor)}
-            </p>
-            <p className="border-r  px-1">게시글 {formatValue(post)}</p>
-            <p className="px-2">회원 수 {formatValue(member)}</p>
+            <p className="px-1">방문자 {count.visitorCount?.count}</p>
+            <p className="px-1">게시글 {count.postCount.count}</p>
+            <p className="px-2">회원 수 {count.memberCount.count}</p>
          </div>
       </aside>
    )
