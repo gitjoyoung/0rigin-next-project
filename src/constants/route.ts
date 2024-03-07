488f5b3bd9670d1b3ea0ca90771a1fc78bb23dba
Jo Young <githuborigin@gmail.com>
2024-03-07 20:13:37 +0900

    feat: 라우트 상수 관리

---
+++ src/constants/route.ts
@@ -0,0 +1,21 @@
+export const ROUTES = {
+   HOME: '/',
+   //    회원가입
+   LOGIN: '/login',
+   FORGET: '/login/forget',
+   SIGN: '/sign',
+   MYPAGE: '/mypage',
+   //    게시판 관련
+   BOARD: '/board',
+   BOARD_READ: '/board/read',
+   BOARD_WRITE: '/board/write',
+   BOARD_UPDATE: '/board/update',
+   // 검색
+   SEARCH: '/search',
+   //    소개
+   INTRODUCE: '/introduce',
+   // 연락
+   CONTACT: '/contact',
+   // 기타 에러페이지
+   NOT_FOUND: '/404',
+}
