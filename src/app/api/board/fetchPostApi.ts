92c11b10f1b3dac6efd6bb42a18ee39aa32a0464
Jo Young <githuborigin@gmail.com>
2024-03-08 22:13:27 +0900

    fix: 게시글 통신 수정

--- src/app/api/board/fetchPostApi.ts
+++ src/app/api/board/fetchPostApi.ts
@@ -7,36 +7,26 @@ import {
    limit,
    orderBy,
    query,
+   startAt,
 } from 'firebase/firestore'
 import { db } from '@/lib/firebase'
-
-interface Post {
-   id: string
-   nickname: string
-   content: string
-   createdAt: Date
-   number: number
-   title: string
-   category: string
-}
+import { Post } from '@/types/boardTypes'
 
 /**
- * 글 전체 목록 가져오기
+ * 최신 글 20개의 목록 가져오기
  * @returns
  */
-export const fetchPosts = async () => {
-   // 'posts' 컬렉션에 대한 쿼리를 생성합니다.
-   // timestamp 필드를 기준으로 내림차순으로 정렬하고, 20개 문서로 제한합니다.
+export const fetchPosts = async (lastDocSnapshot = 1) => {
    const postsQuery = query(
       collection(db, 'posts'),
-      orderBy('number', 'desc'),
+      orderBy('number', 'asc'),
+      startAt(lastDocSnapshot),
       limit(20),
    )
 
    try {
       // 쿼리를 실행하여 문서 스냅샷을 가져옵니다.
       const querySnapshot = await getDocs(postsQuery)
-
       // 각 문서의 데이터를 배열에 저장합니다.
       const posts: Post[] = querySnapshot.docs.map((snapshot) => ({
          id: snapshot.id,
@@ -45,26 +35,26 @@ export const fetchPosts = async () => {
          createdAt: snapshot.data().createdAt.toDate(),
          number: snapshot.data().number,
          title: snapshot.data().title,
-         category: snapshot.data().category || 'any', // Include the missing category property
+         category: snapshot.data().category, // Include the missing category property
       }))
-      console.log('posts : ', posts)
+
       // 가져온 데이터를 반환하거나 상태에 저장합니다.
-      return posts
+      return posts.reverse()
    } catch (error) {
       console.error('Error fetching posts:', error)
       // 오류 처리를 여기서 수행합니다.
+      return null
    }
 }
 
 /**
- * 특정 게시물의 데이터를 조회합니다.
- * @param {string} postId 조회할 게시물의 ID입니다.
+ * 단일 게시물의 데이터를 조회합니다.
+ * @param {string} postID 조회할 게시물의 ID입니다.
  * @returns {Promise<Object>} 조회된 게시물의 데이터를 반환합니다.
  */
-export const fetchPostById = async (postId) => {
-   console.log('postId :', postId)
-   // 'posts' 컬렉션에서 주어진 postId를 가진 문서에 대한 참조를 생성합니다.
-   const postRef = doc(db, 'posts', postId)
+export const fetchPostById = async (postID: string): Promise<Post | null> => {
+   // 'posts' 컬렉션에서 주어진 postID를 가진 문서에 대한 참조를 생성합니다.
+   const postRef = doc(db, 'posts', postID)
 
    try {
       // 문서 참조를 사용하여 문서 스냅샷을 가져옵니다.
@@ -73,43 +63,37 @@ export const fetchPostById = async (postId) => {
       // 문서가 존재하는 경우, 문서 데이터와 함께 문서 ID를 반환합니다.
       if (docSnap.exists()) {
          const postData = docSnap.data()
-         // Post 타입으로 데이터 구성
-         const post: Post = {
+         const createdAt = postData.createdAt.toDate()
+         return {
             id: docSnap.id,
             nickname: postData.nickname,
             content: postData.content,
-            createdAt: postData.createdAt.toDate(),
             number: postData.number,
             title: postData.title,
-            category: postData.category || 'any',
+            createdAt,
          }
-         return post
       }
       return null
    } catch (error) {
-      // 오류가 발생한 경우, 오류를 콘솔에 기록합니다.
       console.error('Error fetching post:', error)
       throw error // 또는 오류를 호출자에게 전파합니다.
    }
 }
 
 /**
- * 개발중...
- * 배너 영역에 들어갈 인기글 특정 조건에 맞는 최대 5개의 게시물 가져오기
+ * 배너 영역에 들어갈 추천 게시물 가져오기
  * @param {string} condition 특정 조건
  * @returns {Promise<Post[]>} 조건에 맞는 게시물 배열을 반환합니다.
  */
 export const fetchTopPosts = async () => {
    // 'posts' 컬렉션에 대한 쿼리를 생성합니다.
    // 특정 조건을 추가하고, 5개 문서로 제한합니다.
+   // desc 내림차순은 큰 숫자부터 작은 숫자로 정렬
    const postsQuery = query(
       collection(db, 'posts'),
-      orderBy('number', 'desc'),
+      orderBy('like', 'desc'),
       limit(5),
-      // Add your specific condition here
-      // 예시: where('category', '==', condition)
    )
-
    try {
       // 쿼리를 실행하여 문서 스냅샷을 가져옵니다.
       const querySnapshot = await getDocs(postsQuery)
@@ -122,13 +106,13 @@ export const fetchTopPosts = async () => {
          createdAt: snapshot.data().createdAt.toDate(),
          number: snapshot.data().number,
          title: snapshot.data().title,
-         category: snapshot.data().title || 'any',
+         category: snapshot.data().category,
       }))
-
       // 가져온 데이터를 반환하거나 상태에 저장합니다.
       return posts
    } catch (error) {
       console.error('Error fetching posts:', error)
       // 오류 처리를 여기서 수행합니다.
+      return null
    }
 }
