faa0e5214f74818c9a4ee5ecb35d034915cfb6b0
Jo Young <githuborigin@gmail.com>
2024-03-15 08:23:46 +0900

    doc: 라우터 주석 추가

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,18 +1,22 @@
 export const ROUTES = {
    HOME: '/',
-   //    회원가입
+   //    로그인
    LOGIN: '/login',
+   // 비밀번호 찾기
    FORGET: '/login/forget',
+   // 회원가입 약관
    SIGN: '/sign',
+   // 회원가입
    SIGN_UP: '/sign/signup',
+   //   마이페이지
    MYPAGE: '/mypage',
-   //    게시판 관련
+   //    게시판
    BOARD: '/board',
    BOARD_WRITE: '/board/write',
    BOARD_UPDATE: '/board/update',
    // 검색
    SEARCH: '/search',
-   //    소개
+   // 소개
    INTRODUCE: '/introduce',
    // 연락
    CONTACT: '/contact',
