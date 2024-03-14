4294d5d5fd266dce06aa7aa8672e906084d0968c
Jo Young <githuborigin@gmail.com>
2024-03-15 08:55:01 +0900

    fix : 깃 대소문자 오류 수정

--- src/app/introduce/page.tsx
+++ src/app/introduce/page.tsx
@@ -1,7 +1,7 @@
-import Introduce from '@/components/Introduce/Introduce'
 import React from 'react'
 import { TickerCounts } from '@/types/tickerTypes'
 import { fetchTickerCounts } from '../api/board/tickerApi'
+import Introduce from '@/components/Introduce/Introduce'
 
 export default async function page() {
    const counts: TickerCounts = await fetchTickerCounts()
