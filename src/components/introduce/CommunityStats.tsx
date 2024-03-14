aec601ca730ee7cf88bae53df3341c19217e0f2c
Jo Young <githuborigin@gmail.com>
2024-03-14 21:59:44 +0900

    refactor : 소개   컴포넌트 리팩토링

--- src/components/introduce/CommunityStats.tsx
+++ src/components/introduce/CommunityStats.tsx
@@ -1,25 +1,54 @@
 import React from 'react'
-import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import {
+   faComments,
+   faEye,
+   faUserFriends,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { TickerCounts } from '@/types/tickerTypes'
 
-export default function CommunityStats({ infoData }) {
-   console.log('infoData', infoData)
+interface Props {
+   counts: TickerCounts
+}
+
+function StatCard({ icon, title, count }) {
+   return (
+      <div className="border rounded-md border-white shadow-lg">
+         <div className="p-4 sm:p-6 flex-col  flex-wrap  items-center text-xs sm:text-sm  justify-center">
+            <FontAwesomeIcon icon={icon} />
+            <div className="flex flex-col ">
+               <p>{title}</p>
+               <p className="text-lg">{count}</p>
+            </div>
+         </div>
+      </div>
+   )
+}
+export default function CommunityStats({ counts }: Props) {
    const stats = [
-      { id: 'posts_and_comments', title: '게시글', count: '1200명' },
-      { id: 'members', title: '회원수', count: '1300명' },
-      { id: 'daily_visitors', title: '일일 방문자', count: '1030명' },
+      {
+         id: 'posts_and_comments',
+         title: '게시글',
+         count: counts?.post,
+         icon: faComments,
+      },
+      {
+         id: 'members',
+         title: '회원수',
+         count: counts?.user,
+         icon: faUserFriends,
+      },
+      {
+         id: 'daily_visitors',
+         title: '일일 방문',
+         count: counts?.visit,
+         icon: faEye,
+      },
    ]
-
    return (
       <div className="grid grid-cols-3 gap-3 m-2 text-sm">
-         {stats.map(({ id, title, count }) => (
-            <div key={id} className="border rounded-md border-white shadow-lg">
-               <div className="p-4">
-                  <FontAwesomeIcon icon={faUsers} />
-                  <p>{title}</p>
-                  <p className="text-lg">{count}</p>
-               </div>
-            </div>
+         {stats.map(({ id, title, count, icon }) => (
+            <StatCard key={id} icon={icon} title={title} count={count} />
          ))}
       </div>
    )
--- src/components/introduce/Introduce.tsx
+++ src/components/introduce/Introduce.tsx
@@ -1,12 +1,17 @@
 'use client'
 
-import Image from 'next/image'
 import React, { useRef } from 'react'
 import INTRODUCE_TEXT from '@/constants/introduce/introduceText'
+import { TickerCounts } from '@/types/tickerTypes'
 import CommunityStats from './CommunityStats'
+import ThreeScene from './ThreeScene'
 
-export default function Introduce({ infoData }) {
-   const scrollToSection = (ref: React.MutableRefObject<null>) => {
+interface Props {
+   counts: TickerCounts
+}
+
+export default function Introduce({ counts }: Props) {
+   const scrollToSection = (ref) => {
       ref.current.scrollIntoView({
          behavior: 'smooth',
          block: 'start',
@@ -24,53 +29,56 @@ export default function Introduce({ infoData }) {
       bg-gradient-to-r from-blue-500 via-sky-400 to-blue-500 text-center text-white relative"
       >
          {/* 마스코트 이미지와 환영 인사 */}
+         <div className="max-w-4xl">
+            <div
+               ref={sectionsRef.story}
+               className="flex flex-col items-center max-w-[600px] gap-3 h-[100vh] 
+               justify-center text-center animate-fadeIn"
+            >
+               <div className="min:h-[140px] h-[180px]">
+                  <ThreeScene />
+               </div>
+               <h1 className="text-2xl font-bold">
+                  {INTRODUCE_TEXT.story.title}
+               </h1>
+               <p className="break-words break-keep px-3">
+                  {INTRODUCE_TEXT.story.body}
+               </p>
+            </div>
 
-         <div
-            ref={sectionsRef.story}
-            className="flex flex-col items-center max-w-[600px] gap-3 h-[100vh] justify-center  text-center animate-fadeIn "
-         >
-            <Image
-               src="/mascot/winksaurus.png"
-               alt="winksaurus"
-               width={300}
-               height={200}
-            />{' '}
-            <h1 className="text-2xl font-bold">{INTRODUCE_TEXT.story.title}</h1>
-            <p className="break-words break-keep px-3">
-               {INTRODUCE_TEXT.story.body}
-            </p>
-         </div>
+            {/*  환영 인사 */}
+            <div
+               ref={sectionsRef.welcome}
+               className="flex flex-col items-center justify-center max-w-[600px] gap-3 h-[100vh]"
+            >
+               <h1 className="text-2xl font-bold px-3">
+                  {INTRODUCE_TEXT.welcome.title}
+               </h1>
+               <p className="break-words break-keep px-3">
+                  {INTRODUCE_TEXT.welcome.body}
+               </p>
+            </div>
 
-         {/*  환영 인사 */}
-         <div
-            ref={sectionsRef.welcome}
-            className="flex flex-col items-center justify-center max-w-[600px] gap-3 h-[100vh]"
-         >
-            <h1 className="text-2xl font-bold px-3">
-               {INTRODUCE_TEXT.welcome.title}
-            </h1>
-            <p className="break-words break-keep px-3">
-               {INTRODUCE_TEXT.welcome.body}
-            </p>
+            {/* 숫자로 보는 0rigin */}
+            <div
+               ref={sectionsRef.numbers}
+               className="flex flex-col items-center justify-center gap-3 w-full h-[100vh]"
+            >
+               <h1 className="text-2xl font-bold px-3">
+                  {INTRODUCE_TEXT.numbers.title}
+               </h1>
+               <p className="px-3"> {INTRODUCE_TEXT.numbers.body} </p>
+               <p>마지막 업데이트: </p>
+               <CommunityStats counts={counts} />
+            </div>
          </div>
-
-         {/* 숫자로 보는 0rigin */}
-         <div
-            ref={sectionsRef.numbers}
-            className="flex flex-col items-center justify-center gap-3 w-full h-[100vh]"
-         >
-            <h1 className="text-2xl font-bold px-3">
-               {INTRODUCE_TEXT.numbers.title}
-            </h1>
-            <p className="px-3"> {INTRODUCE_TEXT.numbers.body} </p>
-            <p>마지막 업데이트: 24.04.23</p>
-            <CommunityStats infoData={infoData} />
-         </div>
-         <nav className=" bottom-10 mr-5 self-end sticky ">
-            <div className="flex flex-col gap-1">
+         <nav className="bottom-2 self-end mr-3 block sticky">
+            <div className="flex flex-col gap-1 text-xs sm:text-sm">
                {Object.entries(sectionsRef).map(([key, ref]) => (
                   <button
-                     className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-300 focus:outline-none focus:ring border-white hover:scale-105 transition-transform duration-300"
+                     className="bg-blue-500 text-white px-3 py-2 rounded-md 
+                     hover:bg-blue-300 focus:outline-none focus:ring 
+                     border-white hover:scale-105 transition-transform duration-300"
                      key={key}
                      type="button"
                      onClick={() => scrollToSection(ref)}
