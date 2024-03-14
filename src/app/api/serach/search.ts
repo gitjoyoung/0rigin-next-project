9e939cea4d57f864b48e2ecd61466f9cc2bebc6c
Jo Young <githuborigin@gmail.com>
2024-03-14 22:22:35 +0900

    fix : 검색 api 설정

--- src/app/api/serach/search.ts
+++ src/app/api/serach/search.ts
@@ -1,15 +1,26 @@
-/** 검색 API
- * @param {string} search
- */
-export default async function fetchSearch(search) {
+import { db } from '@/lib/firebase'
+import { SearchResult } from '@/types/searchTypes'
+import { collection, query, where, getDocs } from 'firebase/firestore'
+
+export default async function fetchSearch(
+   search: string,
+): Promise<SearchResult[]> {
    try {
-      const res = await fetch(
-         `${process.env.NEXT_PUBLIC_API_URL}search?search=${search}`,
-      )
-      const data = await res.json()
-      return data
+      const postsRef = collection(db, 'posts') // 'posts' 컬렉션을 참조
+      // 검색어를 사용하여 쿼리를 생성
+      const q = query(postsRef, where('title', '==', search)) // 'title' 필드가 검색어와 일치하는 문서를 쿼리
+      // 쿼리 실행
+      const querySnapshot = await getDocs(q)
+
+      // 쿼리 결과를 배열로 변환하여 반환
+      return querySnapshot.docs.map((doc) => ({
+         id: doc.id, // 문서 ID
+         title: doc.data().title, // 문서의 title 속성
+         content: doc.data().content, // 문서의 content 속성
+         createdAt: doc.data().createdAt, // 문서의 createdAt 속성
+      })) as SearchResult[] // Explicitly define the type of the returned array as SearchResult[]
    } catch (error) {
       console.log('error :', error)
+      return []
    }
-   return null
 }
