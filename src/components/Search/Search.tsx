452f7f6b88f98fc4bd42b74f08bf22cf8ac497db
Jo Young <githuborigin@gmail.com>
2024-03-14 22:26:50 +0900

    fix : 검색 컴포넌트 수정

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,117 +1,96 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { openai } from '@/lib/openAI'
-import SearchButton from '../Header/SearchBox'
+import fetchSearch from '@/app/api/serach/search'
+import { ROUTES } from '@/constants/route'
+import { SearchResult } from '@/types/searchTypes'
+import SearchBox from '../Header/SearchBox'
 
-interface SearchResult {
-   id: number
-   title: string
-   content: string
-   image: string
-}
 interface Props {
-   placeholder?: string
    decodeSlug?: string
 }
 
-export default function Search({ placeholder, decodeSlug }: Props) {
-   console.log('decodeSlug', decodeSlug)
-
-   const searchResults = [
-      {
-         id: 1,
-         title: 'Custom Title 1ddddddddddd dddddddddddddddddddd',
-         content:
-            'Custom Content 1 with 30 charactersdddddddddddㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ ddddddddddddddddddddddddd',
-         image: '/mascot/winksaurus.png',
-      },
-      {
-         id: 2,
-         title: 'Custom Title 2',
-         content:
-            'Custom Content 2 with 30 charactersㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ',
-         image: '/mascot/winksaurus.png',
-      },
-      {
-         id: 3,
-         title: 'Custom Title 3',
-         content: 'Custom Content 3 with 30 characterㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇs',
-         image: '/mascot/winksaurus.png',
-      },
-   ]
+export default function Search({ decodeSlug }: Props) {
+   const [Result, setResult] = useState<SearchResult[]>([])
    const [GptResult, setGptResult] = useState([])
-   const chatGpt = async () => {
-      const stream = await openai.chat.completions.create({
-         model: 'gpt-3.5-turbo',
-         messages: [
-            {
-               role: 'user',
-               content: decodeSlug,
-            },
-         ],
-         stream: true,
-      })
-      const data = []
 
-      for await (const chunk of stream) {
-         const newData = chunk.choices[0]?.delta?.content || ''
-         setGptResult((prev) => [...prev, newData])
+   useEffect(() => {
+      const chatGpt = async () => {
+         const stream = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+               {
+                  role: 'user',
+                  content: '한글로만 답변해라',
+               },
+               {
+                  role: 'user',
+                  content: decodeSlug,
+               },
+            ],
+            stream: true,
+         })
+         // eslint-disable-next-line no-restricted-syntax
+         for await (const chunk of stream) {
+            const newData = chunk.choices[0]?.delta?.content || ''
+            setGptResult((prev) => [...prev, newData])
+         }
       }
-   }
+      chatGpt()
+      fetchSearch(decodeSlug).then((data) => {
+         setResult(data)
+      })
+   }, [decodeSlug])
 
    return (
       <div>
-         <ul className="py-2  ">
-            <button
-               className="border p-1 border-black"
-               type="button"
-               onClick={chatGpt}
-            >
-               GPT-4 검색결과 버튼
-            </button>
-            <p>{GptResult}</p>
-
-            <div className="flex  overflow-hidden whitespace-nowrap  items-end">
-               <h1 className=" line-clamp-1  min-w-0 ">
+         <ul className="py-2 px-2 flex-col gap-3">
+            <div className="flex justify-center">
+               <h1 className=" line-clamp-1  min-w-0 text-2xl ">
                   검색어 : {decodeSlug}
                </h1>
-               <h1 className="ml-2 font-bold text-2xl ">
-                  게시판 검색결과 {searchResults.length} 건
-               </h1>
+            </div>
+            <div className=" flex-col gap-2 py-2">
+               <h2 className="p-1 font-bold text-xl ">GPT-4 검색결과</h2>
+               <div className=" p-2 whitespace-pre-line border-dotted rounded-lg border-black border">
+                  <p>{GptResult}</p>
+               </div>
+            </div>
+            <div className="flex flex-col py-2  overflow-hidden whitespace-nowrap  ">
+               <h2 className="p-1 font-bold text-xl ">
+                  게시판 검색결과 {Result.length} 건
+               </h2>
             </div>
 
-            {searchResults.map((result: SearchResult) => (
-               <li className=" border p-2 flex" key={result.id}>
+            {Result.map((result: SearchResult) => (
+               <li className=" border p-2 flex gap-3" key={result.id}>
                   <div
                      className="border border-black min-w-[100px] min-h-[100px]
                  items-center flex relative"
                   >
                      <Image
-                        src={result.image}
+                        src={result?.thumbnailUrl || '/sadpepe.jpg'}
                         fill
                         alt={result.title}
                         className="object-cover"
                      />
                   </div>
-                  <div className=" p-1 ">
-                     <Link
-                        className="line-clamp-1  text-base font-semibold "
-                        href="/"
-                     >
-                        {result.title}
-                     </Link>
-                     <Link className="line-clamp-3 text-sm" href="/">
-                        {result.content}
+                  <div className=" p-1 flex flex-col gap-2">
+                     <Link href={`${ROUTES.BOARD}/1/${result.id}`}>
+                        <h1 className="line-clamp-1  text-base font-semibold whitespace-pre overflow-hidden ">
+                           {result.title}
+                        </h1>
+                        <p className="line-clamp-3 text-sm">{result.content}</p>
                      </Link>
                   </div>
                </li>
             ))}
          </ul>
          <div className="m-3 justify-end flex">
-            <SearchButton placeholder={placeholder} />
+            <SearchBox />
          </div>
       </div>
    )
