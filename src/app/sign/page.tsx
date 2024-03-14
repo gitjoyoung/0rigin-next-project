dd5b4ac8990b7f8727dba91f07ed78d656a0e222
Jo Young <githuborigin@gmail.com>
2024-03-14 22:11:56 +0900

    fix: 회원가입 라우터 개선

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -1,35 +1,21 @@
+import { SIGN_UP_TERMS } from '@/constants/auth/signUpTermsText'
 import Link from 'next/link'
 
 function SignPage() {
-   const content = {
-      welcomeMessage: `오리진 커뮤니티에 오신 것을 환영합니다 !`,
-      registration: {
-         question: '회원가입을 진행 하시겠습니까?',
-         alreadyMemberMessage: '이미 회원이신가요?',
-      },
-      login: {
-         href: '/login',
-         buttonText: '이미 회원입니다.',
-      },
-      signup: {
-         href: 'sign/signup',
-         buttonText: '네 좋아요 !',
-      },
-   }
-
    return (
+      //  회원가입 이동 페이지
       <div className="mt-20 mb-20 h-full w-full pr-10 pl-10 flex flex-col items-center gap-5 justify-center">
-         <h1 className="font-bold text-3xl">{content.welcomeMessage}</h1>
-         <h3>{content.registration.question}</h3>
+         <h1 className="font-bold text-3xl">{SIGN_UP_TERMS.welcomeMessage}</h1>
+         <h3>{SIGN_UP_TERMS.registration.question}</h3>
          <div className="flex gap-8 items-center">
-            <Link href={content.login.href}>
+            <Link href={SIGN_UP_TERMS.login.href}>
                <button type="button" className="p-2">
-                  {content.login.buttonText}
+                  {SIGN_UP_TERMS.login.buttonText}
                </button>
             </Link>
-            <Link href={content.signup.href}>
+            <Link href={SIGN_UP_TERMS.signup.href}>
                <button type="button" className="p-2 font-semibold">
-                  {content.signup.buttonText}
+                  {SIGN_UP_TERMS.signup.buttonText}
                </button>
             </Link>
          </div>
