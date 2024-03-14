b6c43c1be7642dac83e526a19578ddf6418a5341
Jo Young <githuborigin@gmail.com>
2024-03-14 22:10:36 +0900

    fix: 검색 라우트 개선

--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -2,17 +2,12 @@ import Search from '@/components/Search/Search'
 import React from 'react'
 
 export default function page({ params }: { params: { slug: string } }) {
+   // 파라미터에서 slug 추출
    const { slug } = params
-   const decodeSlug = decodeURIComponent(slug)
+   // 검색어 그대로 사용하기 위해 디코딩
+   const decodeSlug = decodeURIComponent(slug).replace(/\+/g, ' ')
 
    // Perform search logic here and store the results in a variable
 
-   return (
-      <div>
-         <h1 className="p-2 ">
-            검색어 : <strong className="font-bold">{decodeSlug}</strong>
-         </h1>
-         <Search decodeSlug={decodeSlug} />
-      </div>
-   )
+   return <Search decodeSlug={decodeSlug} />
 }
