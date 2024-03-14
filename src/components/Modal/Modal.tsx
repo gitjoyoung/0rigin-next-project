fd3d8ed076ad9c72155dca4aa0f8eac6a7544abb
Jo Young <githuborigin@gmail.com>
2024-03-14 22:36:40 +0900

    feat: 전역 모달 설정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, SetStateAction } from 'react'
 import { createPortal } from 'react-dom'
 import { useRouter } from 'next/navigation'
+import { ROUTES } from '@/constants/route'
 
 type ModalState = Dispatch<SetStateAction<boolean>>
 interface ModalProps {
@@ -8,7 +9,7 @@ interface ModalProps {
    readonly setOpen: ModalState
 }
 function UI({ setOpen }: { setOpen: ModalState }) {
-   const { push } = useRouter()
+   const router = useRouter()
    return (
       <div className="absolute top-0 flex flex-col items-center justify-center w-full h-[100vh] bg-opacity-45 bg-gray-500 z-10 ">
          <div className="p-20 bg-white border shadow-2xl border-black border-solid rounded-md">
@@ -18,11 +19,11 @@ function UI({ setOpen }: { setOpen: ModalState }) {
                   type="button"
                   className="border border-black border-solid rounded-md p-3.5 bg-white hover:font-bold "
                   onClick={() => {
-                     push('/')
+                     router.push(ROUTES.HOME)
                      setOpen(false)
                   }}
                >
-                  <span className="text-inherit">지원하기</span>
+                  <span className="text-inherit">확인</span>
                </button>
                <button
                   type="button"
