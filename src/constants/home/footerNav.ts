1c55fb214779f9fce67dcf357f1ce8fe553be2ab
Jo Young <githuborigin@gmail.com>
2024-03-15 08:24:20 +0900

    doc: 모든 경로 contact  로 변경

--- src/constants/home/footerNav.ts
+++ src/constants/home/footerNav.ts
@@ -1,7 +1,9 @@
+import { ROUTES } from '../route'
+
 const FOOTER_NAV = Object.freeze([
-   { id: 'inquiry', name: '1:1문의', link: '/' },
-   { id: 'customerService', name: '고객센터', link: '/' },
-   { id: 'contact', name: 'contact', link: '/contact' },
+   { id: 'inquiry', name: '1:1문의', link: ROUTES.CONTACT },
+   { id: 'customerService', name: '고객센터', link: ROUTES.CONTACT },
+   { id: 'contact', name: 'contact', link: ROUTES.CONTACT },
 ])
 
 export default FOOTER_NAV
