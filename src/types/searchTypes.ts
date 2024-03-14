83ec1bb7b35cbc5e51637e4693d2b50b6a4e595f
Jo Young <githuborigin@gmail.com>
2024-03-14 16:42:49 +0900

    feat: search  타입 선언

---
+++ src/types/searchTypes.ts
@@ -0,0 +1,12 @@
+interface SearchResult {
+   id: string
+   title: string
+   content: string
+   createdAt: string
+   nickname?: string
+   number?: number
+   password?: string
+   thumbnailUrl?: string
+}
+
+export type { SearchResult }
