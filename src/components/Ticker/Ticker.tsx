0701b380e4641a700f68ca93416043e00f4405fb
Jo Young <githuborigin@gmail.com>
2024-03-14 22:27:57 +0900

    fix : 티거 컴포넌트 개선

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,28 +1,23 @@
 'use client'
 
-import { fetchInfo } from '@/app/api/board/infoApi'
+import { fetchTickerCounts } from '@/app/api/board/tickerApi'
 import React, { useEffect, useState } from 'react'
 
 export default function Ticker() {
    const [backAnimation, setBackAnimation] = useState('')
    const [count, setCount] = useState({
-      visitorCount: { count: 0 },
-      postCount: { count: 0 },
-      memberCount: { count: 0 },
+      post: 0,
+      visit: 0,
+      user: 0,
    })
 
    const fetchData = async () => {
-      try {
-         const data = await fetchInfo()
-         console.log('data', data)
-         // setCount(data) 이전 데이터를 유지하면서 새로운 데이터를 추가
-         setCount((prev) => ({ ...prev, ...data }))
-         console.log('count', count)
-      } catch (error) {
-         console.error('Failed to fetch data:', error)
-      }
+      const data = await fetchTickerCounts()
+      if (data === null) return
+      // setCount(data) 이전 데이터를 유지하면서 새로운 데이터를 추가
+      setCount((prev) => ({ ...prev, ...data }))
    }
-   // css
+   // css 애니메이션
    useEffect(() => {
       fetchData()
       setBackAnimation('animate-pulse ')
@@ -38,9 +33,9 @@ export default function Ticker() {
          className={` flex gap-2 justify-end ${backAnimation} bg-black fill-in-animation items-center w-full text-white text-xs  `}
       >
          <div className="flex ">
-            <p className="px-1">방문자 {count.visitorCount?.count}</p>
-            <p className="px-1">게시글 {count.postCount.count}</p>
-            <p className="px-2">회원 수 {count.memberCount.count}</p>
+            <p className="px-1">방문자 {count.visit}</p>
+            <p className="px-1">게시글 {count.post}</p>
+            <p className="px-2">회원 수 {count.user}</p>
          </div>
       </aside>
    )
