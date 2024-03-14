07bc975114a37abce7c4e2f9496b221ec949b317
Jo Young <githuborigin@gmail.com>
2024-03-14 22:37:19 +0900

    feat: contact 컴포넌트 수정

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -5,52 +7,85 @@ import {
    faPhoneAlt,
    faMapMarkerAlt,
 } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+import { CONTACT_INFO } from '@/constants/contact/contactNav'
 
 export default function Contact() {
    return (
-      <section className="px-4 py-8 bg-gray-100">
-         <div className="max-w-4xl mx-auto">
-            <h1 className="text-3xl font-semibold text-center mb-8">
-               연락하기
+      <section className="px-4 py-8  h-[80vh] bg-gray-100">
+         <div className="max-w-4xl mx-auto ">
+            <h1 className="text-3xl font-semibold  mb-6">
+               {CONTACT_INFO.title}
             </h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-               {/* 이메일 */}
-               <div className="p-6 bg-white rounded-lg shadow-md">
-                  <div className="flex items-center mb-4">
-                     <FontAwesomeIcon
-                        icon={faEnvelope}
-                        className="text-blue-500 mr-4"
-                        size="2x"
+            <div className="flex flex-wrap  gap-6 w-full ">
+               <div className="flex-1  md:h-auto w-full h-64 rounded-lg overflow-hidden shadow-md bg-black relative">
+                  <picture>
+                     <source
+                        type="image/webp"
+                        srcSet="/webp/winkContact.webp"
                      />
-                     <h2 className="text-xl font-semibold">이메일</h2>
-                  </div>
-                  <p className="text-gray-700">contact@example.com</p>
+                     <Image src="/webp/winkContact.png" fill alt="apple logo" />
+                  </picture>
                </div>
-               {/* 전화번호 */}
-               <div className="p-6 bg-white rounded-lg shadow-md">
-                  <div className="flex items-center mb-4">
-                     <FontAwesomeIcon
-                        icon={faPhoneAlt}
-                        className="text-blue-500 mr-4"
-                        size="2x"
-                     />
-                     <h2 className="text-xl font-semibold">전화번호</h2>
-                  </div>
-                  <p className="text-gray-700">+1 (123) 456-7890</p>
-               </div>
-               {/* 주소 */}
-               <div className="p-6 bg-white rounded-lg shadow-md">
-                  <div className="flex items-center mb-4">
-                     <FontAwesomeIcon
-                        icon={faMapMarkerAlt}
-                        className="text-blue-500 mr-4"
-                        size="2x"
-                     />
-                     <h2 className="text-xl font-semibold">주소</h2>
-                  </div>
-                  <p className="text-gray-700">
-                     123 Main Street, City, Country
-                  </p>
+
+               <div className="grid grid-cols-1  gap-6 md:w-auto  w-full ">
+                  <button
+                     type="button"
+                     className="p-6 bg-white rounded-lg shadow-md"
+                     onClick={() => {
+                        navigator.clipboard.writeText(CONTACT_INFO.email)
+                     }}
+                  >
+                     <div className="flex items-center mb-4 w-full">
+                        <FontAwesomeIcon
+                           icon={faEnvelope}
+                           className="text-blue-500 mr-4"
+                           size="2x"
+                        />
+                        <h2 className="text-xl font-semibold">이메일</h2>
+                     </div>
+                     <p className="text-gray-700 text-left">
+                        {CONTACT_INFO.email}
+                     </p>
+                  </button>
+                  {/* 전화번호 */}
+                  <button
+                     type="button"
+                     className="p-6 bg-white rounded-lg shadow-md"
+                     onClick={() => {
+                        navigator.clipboard.writeText(CONTACT_INFO.phoneNumber)
+                     }}
+                  >
+                     <div className="flex items-center mb-4">
+                        <FontAwesomeIcon
+                           icon={faPhoneAlt}
+                           className="text-blue-500 mr-4"
+                           size="2x"
+                        />
+                        <h2 className="text-xl font-semibold">전화번호</h2>
+                     </div>
+                     <p className="text-gray-700 text-left">
+                        {CONTACT_INFO.phoneNumber}
+                     </p>
+                  </button>
+                  {/* 주소 */}
+                  <button
+                     type="button"
+                     className="p-6 bg-white rounded-lg shadow-md"
+                     onClick={() => {
+                        navigator.clipboard.writeText(CONTACT_INFO.address)
+                     }}
+                  >
+                     <div className="flex items-center mb-4">
+                        <FontAwesomeIcon
+                           icon={faMapMarkerAlt}
+                           className="text-blue-500 mr-4"
+                           size="2x"
+                        />
+                        <h2 className="text-xl font-semibold">주소</h2>
+                     </div>
+                     <p className="text-gray-700 ">{CONTACT_INFO.address}</p>
+                  </button>
                </div>
             </div>
          </div>
