f2b3b8c6a9783fb3b724d38a463c8f986b6462df
Jo Young <githuborigin@gmail.com>
2024-03-14 22:38:28 +0900

    feat: 마이페이지 컴포넌트 생성

---
+++ src/components/Mypage/MyPage.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import React from 'react'
+// import BoardList from '../Board/BoardList'
+
+export default function MyPage() {
+   const { data, status } = useSession()
+   const user = data?.user
+   const { email = '', name = '' } = user || {}
+   console.log('user', user, status)
+
+   return (
+      <div className="flex border border-black rounded-lg p-1 h-[100vh]  justify-between flex-wrap">
+         <div className="border w-96 flex flex-col gap-2 ">
+            <h1 className="text-2xl font-bold">마이페이지</h1>
+            <ul className="mypage-menu">
+               <li className="flex-col flex">
+                  <p className=" text-xs">사용자 이메일 </p>
+                  <span>{email}</span>
+               </li>
+               <li className="flex-col flex">
+                  <p className=" text-xs">사용자 이름 </p>
+                  <span> {name}</span>
+               </li>
+
+               <li>
+                  <button type="button" onClick={() => console.log('asd')}>
+                     로그아웃
+                  </button>
+               </li>
+            </ul>
+         </div>
+
+         <div className="flex-auto bg-gray-300">
+            <h1 className="font-bold">내가 작성한 글</h1>
+            {/* <BoardList boardData={} /> */}
+         </div>
+      </div>
+   )
+}
