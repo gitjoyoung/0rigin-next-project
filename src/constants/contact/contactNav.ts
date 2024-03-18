22713d5dba507cadd413d7671dca9a3c9e5bc0df
Jo Young <githuborigin@gmail.com>
2024-03-18 12:13:13 +0900

    fix : 상수 수정

--- src/constants/contact/contactNav.ts
+++ src/constants/contact/contactNav.ts
@@ -1,6 +1,6 @@
 export const CONTACT_INFO = {
    title: '연락하기',
-   email: '0rigin@0rigin.com 메일 안 보내집니다',
-   phoneNumber: '아직 없어요',
-   address: '서울 어딘가에 살고 있습니다',
+   email: 'githuborigin@gmail.com',
+   phoneNumber: '',
+   address: '서울 ',
 }
