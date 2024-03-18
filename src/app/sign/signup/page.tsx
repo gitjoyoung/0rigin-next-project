5ca3ed092f0802d60ce45fe00cc4b94b1d4d6fa5
Jo Young <githuborigin@gmail.com>
2024-03-18 12:09:59 +0900

    fix : 리액트 에러 수정

--- src/app/sign/signup/page.tsx
+++ src/app/sign/signup/page.tsx
@@ -1,9 +1,5 @@
-'use client'
-
-import Sign from '@/components/Auth/Sign/SignForm'
-import TermsOfService from '@/components/Auth/Sign/TermsOfService'
-import { useState } from 'react'
 import { Metadata } from 'next'
+import SignContent from '@/components/Auth/Sign/SignContent'
 
 export const metadata: Metadata = {
    title: '0rigin 회원가입',
@@ -11,17 +7,5 @@ export const metadata: Metadata = {
 
 export default function Page() {
    // 회원가입 페이지로 이동 여부
-   const [changePage, setChangePage] = useState(false)
-
-   /**
-    * 이용약관 동의 후 회원가입 페이지로 이동
-    * @returns {void}
-    */
-   const handleAcceptTerms = (): void => {
-      setChangePage(true)
-   }
-   if (changePage) {
-      return <Sign />
-   }
-   return <TermsOfService onTermsAgreed={handleAcceptTerms} />
+   return <SignContent />
 }
