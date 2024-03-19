6ae9a3512533049ac1d576891ea1818c2c87063f
Jo Young <githuborigin@gmail.com>
2024-03-19 23:56:33 +0900

    refactor : 포멧팅 함수 개선

--- src/utils/boardValidators/formatNumberWithUnit.ts
+++ src/utils/boardValidators/formatNumberWithUnit.ts
@@ -1,19 +1,12 @@
+// 숫자를 단위에 맞게 포맷팅하는 함수
 export const formatNumberWithUnit = (number: number): string => {
-   let unit = ''
-   let divisor = 1
+   if (!number) return '0'
 
-   if (number >= 100000000) {
-      // 억 단위
-      unit = '억'
-      divisor = 100000000
-   } else if (number >= 10000) {
-      // 만 단위
-      unit = '만'
-      divisor = 10000
-   }
+   const units = ['', '만', '억']
+   const divisors = [1, 10000, 100000000]
 
-   const dividedNumber = number / divisor
-   return dividedNumber % 1 === 0
-      ? `${Math.floor(dividedNumber)}${unit}` // 소수점 이하가 없으면 정수로 반환
-      : `${dividedNumber.toFixed(1)}${unit}` // 소수점 이하가 있으면 한 자리까지 표시
+   const index = Math.floor(Math.log10(number) / 4)
+   const dividedNumber = number / divisors[index]
+
+   return `${dividedNumber.toFixed(1)}${units[index]}`
 }
