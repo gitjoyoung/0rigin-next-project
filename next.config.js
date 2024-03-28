5f92355555648ea12e99ac6c616671ffbeabc30b
Jo Young <githuborigin@gmail.com>
2024-03-28 17:10:36 +0900

    refactor : next config 개선

--- next.config.js
+++ next.config.js
@@ -1,29 +1,33 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')()
+
 const nextConfig = {
-   // sw 국제화 설정 html lang = "ko"
    i18n: {
       locales: ['ko'],
       defaultLocale: 'ko',
    },
-}
-if (process.env.NEXT_PUBLIC_NODE_ENV === 'prod') {
-   nextConfig.compiler = {
-      removeConsole: {
-         exclude: ['error', 'warn'],
-      },
-   }
-}
-
-module.exports = removeImports({
+   images: {
+      domains: ['firebasestorage.googleapis.com'],
+   },
    typescript: {
       ignoreBuildErrors: true,
    },
    eslint: {
       ignoreDuringBuilds: true,
    },
-   ...nextConfig,
    webpack(config, options) {
       return config
    },
-})
+}
+
+// 프로덕션 환경에서 콘솔 로그 제거 설정을 조건부로 추가합니다.
+if (process.env.NEXT_PUBLIC_NODE_ENV === 'prod') {
+   nextConfig.compiler = {
+      ...nextConfig.compiler, // 기존 컴파일러 설정을 유지하면서 추가합니다.
+      removeConsole: {
+         exclude: ['error', 'warn'], // 'error'와 'warn' 로그를 제외하고 모두 제거
+      },
+   }
+}
+
+module.exports = removeImports(nextConfig)
