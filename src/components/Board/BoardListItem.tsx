7240a89bf57091127a4d96fc5f557c7db207e351
Jo Young <githuborigin@gmail.com>
2024-04-04 13:06:22 +0900

    fix : 삭제된 게시물 처리

--- src/components/Board/BoardListItem.tsx
+++ src/components/Board/BoardListItem.tsx
@@ -11,21 +11,35 @@ interface Props {
 }
 
 export default function BoardListItem({ item, page }: Props) {
-   const { id, title, nickname, createdAt, like = 0, views = 0, comment } = item
+   const {
+      id,
+      title,
+      nickname,
+      createdAt,
+      like = 0,
+      views = 0,
+      comment,
+      deleted = false,
+   } = item
    return (
       <section className="flex items-center  flex-wrap p-2  ">
-         <div className="flex-auto gap-2 justify-start flex  w-72">
-            <p className="border text-sm  px-0.5 ">{id}</p>
-            <Link
-               className="flex gap-2 items-center"
-               href={`${ROUTES.BOARD}/${page}/${id}`}
-            >
-               <h1 className="line-clamp-1 ">{title}</h1>
-               <p className="text-gray-400 text-sm mx-1">
-                  {comment && `[${comment}]`}
-               </p>
-            </Link>
-            {/* 조회수 , 추천수가 많은 경우  아이콘 보이기  */}
+         <div className="flex-auto text-sm gap-2 justify-start flex  items-center w-72">
+            <p className="border   px-0.5  ">{id}</p>
+            {deleted ? (
+               <h1 className="line-clamp-1 text-gray-400 ">
+                  삭제된 게시물 입니다.
+               </h1>
+            ) : (
+               <Link
+                  className="flex gap-2 "
+                  href={`${ROUTES.BOARD}/${page}/${id}`}
+               >
+                  <h1 className="line-clamp-1 ">{title}</h1>
+                  <p className="text-gray-400 text-sm mx-1">
+                     {comment && `[${comment}]`}
+                  </p>
+               </Link>
+            )}
          </div>
          <ul className=" flex gap-2 text-xs text-gray-400  ">
             <li className="font-bold">{nickname || '닉네임'}</li>
