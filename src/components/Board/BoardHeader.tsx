589938a41b5b05441264321ae527281679a3f906
Jo Young <githuborigin@gmail.com>
2024-04-07 02:36:02 +0900

    refactor: 게시판 헤더  커스텀 타이틀 적용

--- src/components/Board/BoardHeader.tsx
+++ src/components/Board/BoardHeader.tsx
@@ -1,20 +1,15 @@
-import Link from 'next/link'
 import { ROUTES } from '@/constants/route'
 import BoardMainButton from './Content/BoardMainButton'
+import CustomTitle from '../common/CustomTitle'
 
 export default function BoardHeader({ title }) {
    return (
-      <div
-         className="my-2 flex  justify-between relative
-      items-center  px-1 border-b py-2 border-black"
+      <CustomTitle
+         title="게시글"
+         subTitle="재미있는 게시글을 작성해 보세요"
+         link={ROUTES.BOARD_WRITE}
       >
-         <div className="flex-col flex gap-1">
-            <Link href="/board" className="text-3xl font-bold">
-               {title} 게시판
-            </Link>
-            <p className="text-sm">재미있는 게시글을 작성해 보세요</p>
-         </div>
-         <BoardMainButton route={ROUTES.BOARD_WRITE} title="글쓰기" />
-      </div>
+         <BoardMainButton route={ROUTES.BOARD_WRITE} title={title} />
+      </CustomTitle>
    )
 }
