8dcc6c99330b02f841980388f9e17e49d259f0e0
Jo Young <githuborigin@gmail.com>
2024-04-07 02:35:12 +0900

    feat: 퀴즈 페이지 라우터 설정

--- src/app/(route)/board/[[...slug]]/layout.tsx
+++ src/app/(route)/board/[[...slug]]/layout.tsx
@@ -1,19 +1,23 @@
+'use client'
+
 import {
    fetchLatestPostId,
    fetchPosts,
 } from '@/app/api/board/post/fetchPostApi'
 import BoardContent from '@/components/Board/BoardContent'
+import BoardFooter from '@/components/Board/BoardFooter'
 import BoardHeader from '@/components/Board/BoardHeader'
 import Pagination from '@/components/Board/Pagination'
 import { validateSlug } from '@/utils/slugValidators/slug'
 import React from 'react'
 
 export default async function layout({ children, params }) {
-   console.log(params.slug)
-   const pageSlug = params.slug?.[0]
+   console.log(params)
+   const { slug } = params
+   const pageSlug = slug?.[0]
    const page: number = validateSlug(pageSlug) ? parseInt(pageSlug, 10) : 1
-   const lastPostId = await fetchLatestPostId()
 
+   const lastPostId = await fetchLatestPostId()
    const posts = await fetchPosts(page, lastPostId, 20)
 
    return (
@@ -22,6 +26,7 @@ export default async function layout({ children, params }) {
          {children}
          <BoardContent postData={posts} page={page} />
          <Pagination page={page} lastPostId={lastPostId} />
+         <BoardFooter />
       </>
    )
 }
--- src/app/(route)/quiz/page.tsx
+++ src/app/(route)/quiz/page.tsx
@@ -1,10 +1,9 @@
+import QuizBorad from '@/components/Quiz/QuizBorad'
+
 export default function page() {
    return (
       <div>
-         <h1>퀴즈 페이지 입니다</h1>
-         <div>
-            <p>퀴즈 목록</p>
-         </div>
+         <QuizBorad />
       </div>
    )
 }
