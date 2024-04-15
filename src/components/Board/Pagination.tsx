45a340fc830d8598ba55afc3e7836be63cdd078a
Jo Young <githuborigin@gmail.com>
2024-04-15 20:27:26 +0900

    refactor : 페이지네이션 개선

--- src/components/Board/Pagination.tsx
+++ src/components/Board/Pagination.tsx
@@ -1,36 +1,49 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { ROUTES } from '@/constants/route'
+import { validateSlug } from '@/utils/slugValidators/slug'
+import { fetchLatestPostId } from '@/app/api/board/post/fetchPostApi'
+import { v4 as uuid } from 'uuid'
 
-interface Props {
-   page: number
-   lastPostId: number
-}
+export default function Pagination() {
+   // 마지막 게시글 id 가져오기 id 가 index 이므로 1부터 시작
+   const [lastPostId, setLastPostId] = useState(0)
+   useEffect(() => {
+      async function fetchData() {
+         const fetchedLastPostId = await fetchLatestPostId()
+         setLastPostId(fetchedLastPostId)
+      }
+      fetchData()
+   }, [])
 
-export default function Pagination({ page, lastPostId }: Props) {
-   const totalPages = Math.ceil(lastPostId / 20)
-   // 페이지 구룹 몇개씩 보여줄 것인지
-   const MAX_PAGE = 5
-   // 현재 페이지 그룹 계산 ceil : 올림 현재 페이지 / 전체 페이지
-   const currentPageGroup = Math.ceil(page / MAX_PAGE)
-   // 시작 페이지 및 끝 페이지 계산
-   const startPage = (currentPageGroup - 1) * MAX_PAGE + 1
-   const endPage = Math.min(startPage + MAX_PAGE - 1, totalPages)
+   // 페이지 정보
+   const searchParams = useSearchParams()
+   const page = validateSlug(searchParams.get('page'))
+      ? Number(searchParams.get('page'))
+      : 1
+
+   // 페이지 그룹
+   const POSTS_PER_PAGE = 20 // 한 페이지에 보여줄 게시글 수
+   const PAGES_PER_GROUP = 5 // 한 페이지 그룹에 보여줄 페이지 수
+
+   const totalPages = Math.ceil(lastPostId / POSTS_PER_PAGE) // 전체 페이지 계산
+   const currentPageGroup = Math.ceil(page / PAGES_PER_GROUP) // 현재 페이지 그룹
+   const startPage = (currentPageGroup - 1) * PAGES_PER_GROUP + 1 // 시작 페이지
+   const endPage = Math.min(startPage + PAGES_PER_GROUP - 1, totalPages) // 끝 페이지
    const pageNumbers = Array.from(
       { length: endPage - startPage + 1 },
       (_, i) => startPage + i,
-   )
-
-   const router = useRouter()
+   ) // 페이지 번호 배열
 
    // 페이지 이동
+   const router = useRouter()
    const handlePageChange = (selectPage) => {
-      router.push(`${ROUTES.BOARD}/${selectPage}`)
+      router.push(`${ROUTES.BOARD}?page=${selectPage}`)
    }
 
    return (
@@ -40,7 +53,7 @@ export default function Pagination({ page, lastPostId }: Props) {
             <button
                type="button"
                className="border-none"
-               onClick={() => handlePageChange(startPage - MAX_PAGE)}
+               onClick={() => handlePageChange(startPage - PAGES_PER_GROUP)}
                aria-label="Previous Page Group"
             >
                <FontAwesomeIcon icon={faCaretLeft} />
@@ -49,12 +62,12 @@ export default function Pagination({ page, lastPostId }: Props) {
 
          {/* 현재 페이지 번호 */}
          {pageNumbers.map((index: number) => (
-            <div key={`${index}PAGE`} className="mx-1">
+            <div key={uuid()} className="mx-1">
                {page === index ? (
                   <p className="underline p-1 text-blue-600">{index}</p>
                ) : (
                   <Link
-                     href={`${ROUTES.BOARD}/${index}`}
+                     href={`${ROUTES.BOARD}?page=${index}`}
                      type="button"
                      className="border-none"
                   >
