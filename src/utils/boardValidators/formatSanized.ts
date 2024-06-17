18bb0c9fb69c6a78eba04dedc293f04c68c96e87
Jo Young <githuborigin@gmail.com>
2024-06-18 02:32:40 +0900

    feat : 유틸 함수 추가

--- src/utils/boardValidators/formatSanized.ts
+++ src/utils/boardValidators/formatSanized.ts
@@ -15,3 +15,14 @@ export const sanitized = async (arg) => {
    const resultHtml = String(sanitizedContent.value)
    return resultHtml
 }
+
+export const sanitizeHTML = async (htmlString) => {
+   const sanitizedContent = await unified()
+      .use(remarkParse) // Markdown 문서를 파싱
+      .use(rehypeSanitize) // 살균 처리
+      .use(rehypeStringify) // 문자열로 변환
+      .process(htmlString) // 처리할 HTML 문자열 입력
+
+   const resultHtml = String(sanitizedContent.value)
+   return resultHtml // 살균 처리된 안전한 HTML 문자열 반환
+}
--- src/utils/slugValidators/validatePostQuery.tsx
+++ src/utils/slugValidators/validatePostQuery.tsx
@@ -2,4 +2,6 @@ import { z } from 'zod'
 
 export const validatePostQuery = z
    .string()
-   .regex(/^\d+$/, '슬러그는 숫자여야 합니다.') // 정규식을 이용해 숫자만 포함된 문자열인지 확인
+   .regex(/^\d+$/, 'Must be a numeric string')
+   .transform((n) => parseInt(n, 10))
+   .optional()
