ea69cafa3819a4f9a2ac8782f6356a8890cf94ac
Jo Young <githuborigin@gmail.com>
2024-06-18 02:18:05 +0900

    feat: 미들웨어 추가

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+
+export function middleware(request: NextRequest) {
+   const url = request.nextUrl
+   const { device } = userAgent(request) // userAgent를 사용하여 디바이스 정보 추출
+   const viewport = device.type === 'mobile' ? 'mobile' : 'desktop' // 모바일이면 'mobile', 아니면 'desktop'
+   // console.log('device', device)
+   url.searchParams.set('viewport', viewport) // URL의 쿼리 파라미터로 viewport 설정
+   console.log('viewport', viewport)
+   return NextResponse.rewrite(request.nextUrl)
+}
