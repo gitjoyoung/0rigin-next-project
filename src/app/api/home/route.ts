660684d2716e2f4fc71f8e859a5f81afb751cce3
Jo Young <githuborigin@gmail.com>
2024-06-20 02:03:50 +0900

    fix : 배포시 통신 문제 테스트

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -10,5 +10,7 @@ export async function GET(request: Request) {
    const lastPostId = await fetchLatestPostId()
    const topData = await fetchTopPosts()
    const fetchedPosts = await fetchPosts('1', lastPostId, 20)
-   return Response.json({ topData, fetchedPosts })
+   return new Response(JSON.stringify({ topData, fetchedPosts }), {
+      headers: { 'Content-Type': 'application/json' },
+   })
 }
