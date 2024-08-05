e2d29689d4e0557176577aebafe8a7aaf5254f54
Jo Young <githuborigin@gmail.com>
2024-08-05 13:15:16 +0900

    refactor : 로그인 폼 개선

--- src/app/auth/_components/Login/LoginForm.tsx
+++ src/app/auth/_components/Login/LoginForm.tsx
@@ -1,25 +1,15 @@
 'use client'
 import { ROUTES } from '@/constants/route'
-import { signIn } from 'next-auth/react'
+import { signInWithCredentials } from '@/serverAction/auth'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useFormState } from 'react-dom'
 
-export default function Login({ handleLoginSubmit }) {
-   const [email, setEmail] = useState('')
-   const [password, setPassword] = useState('')
+export default function Login() {
+   const [error, setError] = useState('')
+   const [pending, setPending] = useState(false)
 
-   const handleSubmit = async () => {
-      await handleLoginSubmit({
-         email,
-         password,
-      })
-   }
-   const onChangeEmail = (e: any) => {
-      setEmail(e.target.value)
-   }
-   const onChangePassword = (e: any) => {
-      setPassword(e.target.value)
-   }
+
    return (
       <section className=" w-full h-full my-auto flex justify-center items-center">
          <div className="flex flex-col h-full border p-10  justify-center items-center gap-6 m-3 rounded-xl">
@@ -28,28 +18,24 @@ export default function Login({ handleLoginSubmit }) {
                <h1 className="font-bold  text-2xl ">로그인</h1>
             </div>
             {/* 입력폼 */}
-            <form className="flex flex-col gap-3">
+            <form
+               className="flex flex-col gap-3"
+               action={signInWithCredentials}
+            >
                <input
                   className="border p-2"
                   type="email"
                   name="email"
-                  onChange={onChangeEmail}
                   placeholder="이메일"
                />
                <input
                   className="border p-2"
                   type="password"
                   name="password"
-                  value={password}
-                  onChange={onChangePassword}
                   placeholder="비밀번호"
                />
-               <p className="text-xs text-red-500 overflow-auto">{}</p>
-               <button
-                  type="button"
-                  className="mt-3 p-2"
-                  onClick={handleSubmit}
-               >
+               <p className="text-xs text-red-500 overflow-auto">{error}</p>
+               <button type="submit" className="mt-3 p-2" disabled={pending}>
                   로그인
                </button>
             </form>
