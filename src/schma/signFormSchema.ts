dae8b3b9bf7a99517d7ed44687fcd9b3e9122344
Jo Young <githuborigin@gmail.com>
2024-08-05 13:34:24 +0900

    refactor : zod 스키마 변경

--- src/schma/signFormSchema.ts
+++ src/schma/signFormSchema.ts
@@ -4,16 +4,12 @@ const genderSchema = z
    .string()
    .regex(/^(남성|여성|기타)$/, '성별을 선택 해주세요')
 
-const userEmailSchema = z
-   .string()
-   .min(4, '아이디 4자 이상이어야 합니다.')
-   .max(11, '아이디는 12자 미만이어야 합니다.')
-   .regex(/^[a-z0-9]+$/, '영어 소문자와 숫자만 허용됩니다.')
+const userEmailSchema = z.string().email('이메일 형식이 아닙니다.')
 
 const passwordSchema = z
    .string()
-   .min(8, '비밀번호는 8자 이상이어야 합니다.')
-   .max(12, '비밀번호는 12자 이하이어야 합니다.')
+   .min(8, '비밀번호는 대소문자 특수문자 포함 8자 이상이어야 합니다.')
+   .max(15, '비밀번호는 대소문자 특수문자 포함 15자 이하이어야 합니다.')
    .regex(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]+$/,
       '비밀번호는 영어 대소문자, 숫자, 특수문자를 포함해야 합니다.',
@@ -25,9 +21,10 @@ const signUpSchema = z
       email: userEmailSchema,
       password: passwordSchema,
       confirmPassword: passwordSchema,
-   }).passthrough()
+   })
+   .passthrough()
    .refine((data) => data.password === data.confirmPassword, {
-      message: '비밀번호와 확인 비밀번호가 일치하지 않습니다.',
+      message: '비밀번호 재확인이 일치하지 않습니다.',
       path: ['confirmPassword'],
    })
 
