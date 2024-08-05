cd0397fdeaf08d7464b062c5a18b3d9eae68abcd
Jo Young <githuborigin@gmail.com>
2024-08-05 13:24:26 +0900

    fix : auth 설정 수정

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials'
 import { signInSchema } from './schma/auth'
 import saltAndHashPassword from './utils/authValidators/password'
 import { ZodError } from 'zod'
-import { fetchSignIn } from './service/auth/signIn'
+import { fetchSignIn } from './service/auth/fetchSignIn'
 
 interface Credentials {
    email: string
@@ -22,28 +22,45 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
          },
          authorize: async (credentials) => {
             try {
-               let user = null
-
+               console.log('credentials', credentials)
                const { email, password } =
                   await signInSchema.parseAsync(credentials)
                // console.log('credentials', email, password)
-               // const pwHash = saltAndHashPassword(password)
+               const pwHash = saltAndHashPassword(password)
 
                // logic to verify if the user exists
-               user = await fetchSignIn(email, password)
+               const user = await fetchSignIn(email, pwHash)
 
                if (!user) {
-                  throw new Error('User not found.')
+                  throw new Error('해당 회원의 정보가 없습니다.')
                }
 
                return user
             } catch (error) {
                if (error instanceof ZodError) {
-                  console.log('ZodError:', error.errors)
+                  throw new Error('검증 오류' + error.errors[0].message)
                }
-               console.error('Authorization error:', error)
+               throw new Error('통신 실패' + error.message)
             }
          },
       }),
    ],
+   callbacks: {
+      async signIn({ user, account, profile }) {
+         if (!user.email) {
+            return false
+         }
+         return true
+      },
+      async jwt({ token, user }) {
+         if (user) {
+            token.email = user.email
+         }
+         return token
+      },
+      async session({ session, token }) {
+         session.user.email = token.email
+         return session
+      },
+   },
 })
