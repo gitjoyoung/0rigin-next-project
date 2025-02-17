21ff7d5269f77fd5ef3c318a3ff70b8b02ec1c96
Jo Young <githuborigin@gmail.com>
2025-02-17 23:17:40 +0900

    fix : 로그인 레이아웃 수정

--- src/app/(auth)/login/ui/LoginForm.tsx
+++ src/app/(auth)/login/ui/LoginForm.tsx
@@ -50,14 +50,14 @@ export default function Login() {
    }
 
    return (
-      <div className="flex flex-col mt-12 items-center justify-center">
+      <div className="flex flex-col mt-12 w-full items-center justify-center">
          <div className="text-center space-y-2 mb-8">
             <h2 className="text-3xl font-bold tracking-tight">환영합니다</h2>
             <p className="text-muted-foreground text-sm">
                {'"모든 위대한 여정은 작은 한 걸음에서 시작됩니다"'}
             </p>
          </div>
-         <Card className="w-[400px]">
+         <Card>
             <CardHeader>
                <CardTitle>로그인</CardTitle>
             </CardHeader>
