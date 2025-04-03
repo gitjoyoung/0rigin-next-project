b5b5d01f1f1b646c3a0055d54f2876af0a652850
gitjoyoung <githuborigin@gmail.com>
2025-04-04 00:36:13 +0900

    fix : 댓글 함수 되돌리기

--- src/app/(content)/board/ui/comment/comment-form.tsx
+++ src/app/(content)/board/ui/comment/comment-form.tsx
@@ -1,23 +1,77 @@
+'use client'
+
+import { commentSchema } from '@/schema/commentSchema'
+import { Button } from '@/shared/shadcn/ui/button'
+import { Input } from '@/shared/shadcn/ui/input'
+import { Textarea } from '@/shared/shadcn/ui/textarea'
+import type { CreateCommentData } from '@/types/commentTypes'
+import { useRouter } from 'next/navigation'
+import React, { useRef } from 'react'
+
 interface Props {
    postId: string
 }
-
 export default function CommentForm({ postId }: Props) {
+   // 댓글 폼 참조
+   const commentFormRef = useRef(null)
+   const { refresh } = useRouter()
+
+   // 필드 초기화
+   const clearFormFields = () => {
+      commentFormRef.current.comment.value = ''
+   }
+   const handleComment = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      const nickname = commentFormRef.current.nickname.value
+      const password = commentFormRef.current.password.value
+      const comment = commentFormRef.current.comment.value
+
+      const commentObject: CreateCommentData = {
+         postId,
+         nickname,
+         password,
+         comment,
+      }
+      const result = commentSchema.safeParse(commentObject)
+      if (result.success === false) {
+         alert(result.error)
+         return
+      }
+      clearFormFields()
+   }
+
    return (
-      <form className="mt-4">
-         <textarea
-            className="w-full p-2 border rounded"
-            placeholder="댓글을 작성하세요"
-            rows={3}
-         />
-         <div className="flex justify-end mt-2">
-            <button
-               type="submit"
-               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-               작성하기
-            </button>
-         </div>
-      </form>
+      <div className="py-2 ">
+         <form
+            ref={commentFormRef}
+            onSubmit={handleComment}
+            className="flex flex-col gap-2"
+         >
+            <div className="flex  gap-2 text-sm ">
+               <Input
+                  name="nickname"
+                  type="text"
+                  autoComplete="off"
+                  placeholder="닉네임"
+                  className="max-w-40 rounded-none"
+               />
+               <Input
+                  name="password"
+                  type="password"
+                  maxLength={10}
+                  autoComplete="off"
+                  placeholder="비밀번호"
+                  className="max-w-40 rounded-none"
+               />
+            </div>
+
+            <Textarea
+               name="comment"
+               className=" border rounded-none"
+               placeholder="댓글 입력"
+            />
+            <Button type="submit">댓글달기</Button>
+         </form>
+      </div>
    )
 }
