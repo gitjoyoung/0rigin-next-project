f21fb055aa46793d3531f00bbc78af1225b994d3
gitjoyoung <githuborigin@gmail.com>
2025-05-20 03:59:38 +0900

    feat : 게시판이름 변경

--- src/app/(content)/board/ui/board-header.tsx
+++ src/app/(content)/board/ui/board-header.tsx
@@ -3,14 +3,15 @@ import { Button } from '@/shared/shadcn/ui/button'
 import Link from 'next/link'
 
 const HEADER_DATA = {
-   title: '철학 게시판',
-   description: '자유로운 생각을 나누어 보세요!',
+   title: '실용 철학 게시판',
+   description:
+      '닭이 먼저냐! 달걀이 먼저냐! 누구나 자유롭게 소통 가능한 게시판입니다.',
    create: '글쓰기',
 }
 
 export default function BoardHeader() {
    return (
-      <article className="my-4 px-1 flex justify-between items-center border-b border-slate-200 pb-4">
+      <article className="m-4 px-1 flex justify-between items-center border-b border-slate-200 pb-4">
          <div className="space-y-1">
             <Link href={ROUTE_BOARD}>
                <h1 className="text-2xl font-bold tracking-tight hover:text-slate-700 transition-colors">
