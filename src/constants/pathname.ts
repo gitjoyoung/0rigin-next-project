27b5d318680b39d36288c52fcf2d9cfd3e11d3ab
gitjoyoung <githuborigin@gmail.com>
2025-10-08 04:18:45 +0900

    fix : 게시글 클릭시 최상단 이동하도록 수정

--- src/constants/pathname.ts
+++ src/constants/pathname.ts
@@ -1,5 +1,5 @@
 // 자동 생성된 경로 상수 - 수정하지 마세요
-// 생성 시간: 2025-10-08 03:25:20
+// 생성 시간: 2025-10-08 04:15:23
 // 동적 라우트([param])는 제외됨
 
 export const ROUTE_FORGET_PASSWORD = "/forget/password";
--- src/feature/board/post/post-view/index.tsx
+++ src/feature/board/post/post-view/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import type { Database } from "@/shared/types";
+import { useEffect } from "react";
 import MarkDownViewer from "./mark-down-viewer";
 import PostHeader from "./post-header";
 
@@ -8,6 +11,10 @@ interface Props {
 }
 
 export default function PostView({ postData, likeCount }: Props) {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [postData.id]);
+
   return (
     <section className="flex flex-col gap-3 ">
       <PostHeader {...postData} likeCount={likeCount} />
